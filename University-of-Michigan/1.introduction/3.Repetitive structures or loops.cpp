33f8676501ad1ba09dc95bb9c9a06eeb6ca9f6f6
sajjad-njr <sajjadrahman1020@gmail.com>
2020-10-23 18:23:44 +0600

    while loop

---
+++ University-of-Michigan/1.introduction/3.Repetitive structures or loops.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    /*while loop */
+
+    int n ;
+    cout<<"Enter a number " ;
+    cin>>n;
+
+    while(n>0)
+    {
+        cout<< n << " ";
+        n--;
+    }
+    cout<<"sanjida" <<endl;
+    //system("pause");
+
+    return 0 ;
+}
