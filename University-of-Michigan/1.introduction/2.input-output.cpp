7d9f4178272c83e239fb1494eddaad7d07068b22
sajjad-njr <sajjadrahman1020@gmail.com>
2020-10-23 18:11:43 +0600

    cin and cout done

---
+++ University-of-Michigan/1.introduction/2.input-output.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+int main()
+{
+    int a;
+    cout << "Please Enter a number : " ;
+
+    cin >> a;
+    cout<<"The insert value is = " << a ;
+
+    cout << "\n\nDouble of a = " << a *2 ;
+    getch();
+    return 0;
+}
