7d074ecf59dd0908d2f59ea3504f4126e80e91c7
sajjad-njr <sajjadrahman1020@gmail.com>
2020-10-24 18:29:18 +0600

    While loop

---
+++ University-of-Michigan/1.introduction/5.do-while-loop.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std ;
+
+ int main()
+ {
+     unsigned long n ;
+     do
+     {
+        cout << "Enter a Number between 1 to 10 :  ";
+        cin >> n ;
+
+        cout << "\n" <<"You Entered : " << n << "\n\n";
+
+     } while( n != 0);
+
+     //system("pause");
+
+     return 0 ;
+ }
+
