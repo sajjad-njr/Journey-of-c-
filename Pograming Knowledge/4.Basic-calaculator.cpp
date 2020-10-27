8c236c95a9ac436a6f313baa1bf81a4f61ca020d
sajjad-njr <sajjadrahman1020@gmail.com>
2020-10-27 22:15:22 +0600

    Basic Operator math

--- Pograming Knowledge/4.Basic-calaculator.cpp
+++ Pograming Knowledge/4.Basic-calaculator.cpp
@@ -4,16 +4,40 @@ using namespace std ;
 int main()
 {
     /*Variable declare*/
-    int x = 9090 ;
-    int z = 8976;
+    //int x = 9090 ;
+    //int z = 8976;
 
-    int sum = x + z + 90 ;
-    int sub = x - z -90;
+    //int sum = x + z + 90 ;
+    //int sub = x - z -90;
+
+    int x , y ;
+
+    cout << " Enter a new number : " ;
+
+    cin >> x ;
+
+    cout << " Enter a new number : " ;
+
+    cin >> y ;
+
+    int add = x + y ;
+
+    int  sub = x - y ;
+
+    int mul = x * y ;
+
+    int vag = x / y ;
+
 
     /*Print option*/
-    cout << "Total sum  = " << sum << endl;
 
-    cout <<"Total sub = " << sub << endl;
+    cout << "Total sum  = " <<  add << endl;
+
+    cout << "Total  sub= " <<  sub << endl;
+
+    cout <<"Total  mul = " <<  mul << endl;
+
+    cout << "Total  vag  = " <<  vag << endl;
 
     return 0 ;
 }
