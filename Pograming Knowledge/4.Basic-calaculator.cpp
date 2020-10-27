e0a98c27807b29c16a73dc32c9bb6b98d216435b
sajjad-njr <sajjadrahman1020@gmail.com>
2020-10-27 21:51:57 +0600

    Basic Operator

---
+++ Pograming Knowledge/4.Basic-calaculator.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std ;
+
+int main()
+{
+    /*Variable declare*/
+    int x = 9090 ;
+    int z = 8976;
+
+    int sum = x + z + 90 ;
+    int sub = x - z -90;
+
+    /*Print option*/
+
+    cout << "\n\tTotal sum  = " << sum ;
+    cout <<"\n\tTotal sub = " << sub ;
+
+    return 0 ;
+}
