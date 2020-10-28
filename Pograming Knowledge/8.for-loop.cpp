ff4a01057f6e0f634a145b4fc3b4dbcc00ffadbe
sajjad-njr <sajjadrahman1020@gmail.com>
2020-10-28 22:53:31 +0600

    for loop

---
+++ Pograming Knowledge/8.for-loop.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std ;
+
+int main()
+{
+    int i = 1 ;
+
+    for ( i ; i < 30 ; i++)
+    {
+        if(i%2 == 0)
+            cout<<i<<endl;
+    }
+
+    return 0 ;
+}
