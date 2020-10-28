5a0b8ebd4dfbdd37778b2134899bf19aa0cb3609
sajjad-njr <sajjadrahman1020@gmail.com>
2020-10-28 22:39:55 +0600

    if else

--- Pograming Knowledge/6.if-else.cpp
+++ Pograming Knowledge/6.if-else.cpp
@@ -1,9 +1,24 @@
 #include<iostream>
 
 using namespace std ;
+
 int main()
 {
-    int agg = 90 ;
+    int age ;
+    //int height ;
 
+    cout<<"Enter your  age : " <<endl;
+    cin >> age ;
 
+    if(age == 20 || age > 20)
+    {
+        cout<<"Your  agae is right " << endl;
+    }
+
+    else
+    {
+         cout<<"Y our  age is  Wrong " << endl;
+    }
+
+    return 0 ;
 }
