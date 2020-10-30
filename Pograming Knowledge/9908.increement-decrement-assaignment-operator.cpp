53d11828be359e68b76b8f35f277dc7499166865
sajjad-njr <sajjadrahman1020@gmail.com>
2020-10-30 22:54:33 +0600

    increment

---
+++ Pograming Knowledge/9908.increement-decrement-assaignment-operator.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std ;
+
+int main()
+{
+    int x = 9;
+    int y = 10 ;
+    x++;
+    y++;
+
+    cout << x++ << endl;
+    cout << ++y << endl;
+
+    return 0 ;
+}
