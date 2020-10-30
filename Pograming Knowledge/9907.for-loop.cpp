5c746eefc1e308d151b2553c33f18800acb42a41
sajjad-njr <sajjadrahman1020@gmail.com>
2020-10-30 23:25:08 +0600

    git push

---
+++ Pograming Knowledge/9907.for-loop.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std ;
+
+int main()
+{
+    int a = 90 ;
+    int b = 45;
+
+    for( int i = 0 ; i <= 10 ; i++)
+    {
+        cout << "The team = " << i << endl;
+    }
+
+    return 0 ;
+}
