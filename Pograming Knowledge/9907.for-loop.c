155dbb5c75810207bcd078406420b4158d9aadc5
sajjad-njr <sajjadrahman1020@gmail.com>
2020-10-30 23:17:55 +0600

    increment  c

---
+++ Pograming Knowledge/9907.for-loop.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+//using namespace std ;
+
+int main()
+{
+    int x = 1 ;
+    int v = 10 ;
+
+    for( int i = 0 ; i < 10 ; i++)
+    {
+        printf("\n%d = ",i);
+    }
+    return 0 ;
+}
