14dc2ce52803bfb436c979b3e5a43ffa6e8c208c
sajjad-njr <sajjadrahman1020@gmail.com>
2020-11-02 23:54:35 +0600

    global video

---
+++ Pograming Knowledge/15.scope-and-varible.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std ;
+
+int galib = 900 ;
+
+ int addVal()
+ {
+     int galib = 200 ;
+ }
+int main()
+{
+    int galib = 90000 ;
+    cout << galib;
+
+    return 0 ;
+}
