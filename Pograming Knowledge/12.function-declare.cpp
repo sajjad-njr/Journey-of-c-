8efbfe20845ac56272b3e2619048ccf7a3ca238d
sajjad-njr <sajjadrahman1020@gmail.com>
2020-11-02 22:15:00 +0600

    functions declare

--- Pograming Knowledge/12.function-declare.cpp
+++ Pograming Knowledge/12.function-declare.cpp
@@ -2,13 +2,17 @@
 
 using namespace std ;
 
+void firstName();
+
+int main()
+{
+    firstName();
+    firstName();
+    return 0 ;
+}
+
 void firstName()
 {
     cout << endl;
     cout << "I  first function " <<endl ;
-}
-int main()
-{
-    firstName();
-    return 0 ;
 }
