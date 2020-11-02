aa5378a87eadd6c13cc55054088212509fbe5ff0
sajjad-njr <sajjadrahman1020@gmail.com>
2020-11-02 23:43:07 +0600

    dafult

---
+++ Pograming Knowledge/14.function-default.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+
+using namespace std ;
+
+int mul(int a = 90 , int b = 12)
+{
+    return  a + b ;
+
+}
+
+int main()
+{
+    int x , y , z ;
+
+    //cin >> x ;
+    //cin >> y ;
+
+    //z = mul(x,y);
+    //cout << mul();
+
+    cout << "This mul = " << mul() << endl;
+
+    return 0 ;
+}
+
+
