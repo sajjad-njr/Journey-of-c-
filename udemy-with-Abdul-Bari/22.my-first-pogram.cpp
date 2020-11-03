661bafdfa8c5dbca0e2606ad090b03715a123a62
sajjad-njr <sajjadrahman1020@gmail.com>
2020-11-03 22:21:27 +0600

    getline use

--- udemy-with-Abdul-Bari/22.my-first-pogram.cpp
+++ udemy-with-Abdul-Bari/22.my-first-pogram.cpp
@@ -5,8 +5,9 @@ int main()
 {
     string name ;
     cout << "May i know your name " ;
-    cin >> name ;
+    //cin >> name ; // before space print all
 
+    getline(cin , name );  //get line print all space until enter
     cout << " Welcome dear " << name ;
 
     return 0 ;
