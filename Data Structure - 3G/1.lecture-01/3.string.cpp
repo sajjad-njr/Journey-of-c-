c282a9399bdfaf9e0a266660c5818a75e5a510d0
sajjad-njr <sajjadrahman1020@gmail.com>
2020-11-03 14:31:48 +0600

    string work

---
+++ Data Structure - 3G/1.lecture-01/3.string.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<string>
+using namespace std ;
+
+int main()
+{
+   string myNfo;
+
+   myNfo = " Ofo is one of the  best friend of Nfo" ;
+
+   cout << myNfo ;
+
+   return 0 ;
+}
