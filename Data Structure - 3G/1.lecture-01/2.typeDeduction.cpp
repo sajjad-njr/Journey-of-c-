c5c86743839e0cf3eaa301c032c5c303f28be5d8
sajjad-njr <sajjadrahman1020@gmail.com>
2020-11-03 14:26:24 +0600

    type de duction not work

---
+++ Data Structure - 3G/1.lecture-01/2.typeDeduction.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std ;
+
+int afa = 0 ;
+
+    auto vai = afa ;
+    vai = 9 ;
+int main ()
+{
+    //int afa = 0 ;
+
+   // auto vai = afa ;
+    //vai = 9 ;
+    cout << "New variable  = " << vai << endl;
+
+    return 0 ;
+}
