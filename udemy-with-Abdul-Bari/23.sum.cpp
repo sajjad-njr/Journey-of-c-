21e233815bd35fc34a69e4390fedfcf34e8fe41f
sajjad-njr <sajjadrahman1020@gmail.com>
2020-11-03 22:24:04 +0600

    sum use

---
+++ udemy-with-Abdul-Bari/23.sum.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std ;
+
+int main()
+{
+    int a , b , c ;
+
+    cout << "Enter two num : " ;
+
+    cin >> a >> b ;
+
+    c = a + b ;
+
+    cout << "Sum = " << c << endl ;
+
+    return 0 ;
+}
