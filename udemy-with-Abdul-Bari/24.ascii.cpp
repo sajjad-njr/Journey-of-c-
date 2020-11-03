ed11af7542fd34abd122aff3e68da40aa7cd7d38
sajjad-njr <sajjadrahman1020@gmail.com>
2020-11-03 22:27:38 +0600

    upper to lower no work

---
+++ udemy-with-Abdul-Bari/24.ascii.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std ;
+
+int main()
+{
+    char a ;
+
+    cout << "Enter any lower case " ;
+    cin >> a ;
+
+    cout << "show upper latter : " << a + 32 ;
+    return 0 ;
+}
