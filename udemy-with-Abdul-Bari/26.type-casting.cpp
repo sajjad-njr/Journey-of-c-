102e8091520a04a48259d6c62baa9841ffe69e91
sajjad-njr <sajjadrahman1020@gmail.com>
2020-11-03 22:47:49 +0600

    type casting

---
+++ udemy-with-Abdul-Bari/26.type-casting.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std ;
+
+int main()
+{
+    int a , b , c ;
+
+    a = 91 ;
+    b = 9 ;
+
+    float d ;
+
+    d = (float) a / b ;
+    //d = a / b ;
+
+    cout << d ;
+
+    return 0 ;
+}
