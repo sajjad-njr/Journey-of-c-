583fd282d86904633d0afb922816dedbd08dd40b
sajjad-njr <sajjadrahman1020@gmail.com>
2020-11-03 22:19:24 +0600

    sting without header string

--- Data Structure - 3G/1.lecture-01/2.typeDeduction.cpp
+++ Data Structure - 3G/1.lecture-01/2.typeDeduction.cpp
@@ -2,16 +2,12 @@
 
 using namespace std ;
 
-int afa = 0 ;
+int main ()
+{
+    int afa = 0 ;
 
     auto vai = afa ;
     vai = 9 ;
-int main ()
-{
-    //int afa = 0 ;
-
-   // auto vai = afa ;
-    //vai = 9 ;
     cout << "New variable  = " << vai << endl;
 
     return 0 ;
---
+++ udemy-with-Abdul-Bari/22.my-first-pogram.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std ;
+int main()
+{
+    string name ;
+    cout << "May i know your name " ;
+    cin >> name ;
+
+    cout << " Welcome dear " << name ;
+
+    return 0 ;
+}
