f7b8905291b6b549226bcbc8323c03b5e06e51b7
sajjad-njr <sajjadrahman1020@gmail.com>
2020-11-03 14:14:01 +0600

    initialized variable

--- Data Structure - 3G/1.lecture-01/1.variavle-intialize.cpp
+++ Data Structure - 3G/1.lecture-01/1.variavle-intialize.cpp
@@ -4,9 +4,9 @@ using namespace std ;
 
 int main()
 {
-    int a(9) ;
-    int b{1} ;
-    int c = 90 ;
+    int a(9) ; //initial declare
+    int b{1} ;  //initial declare
+    int c = 90 ;  //initial declare
 
     cout << "Total = " << a+b+c <<endl ;
 
