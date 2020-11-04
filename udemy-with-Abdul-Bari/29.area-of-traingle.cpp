82e4d66d326628953d69ec2551133d22bd8d8b83
sajjad-njr <sajjadrahman1020@gmail.com>
2020-11-04 19:15:00 +0600

    Area of a Traingle

---
+++ udemy-with-Abdul-Bari/29.area-of-traingle.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std ;
+
+int main()
+{
+    int b , h ;
+
+    cin >> b >> h ;
+
+    float area = ( b * h) / 2 ;
+
+    cout << " Area " << area ;
+
+    return 0 ;
+}
