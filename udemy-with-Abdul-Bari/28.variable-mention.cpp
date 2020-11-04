8f957b8a27ce2dc531bcda9b77758132b81c6fc3
sajjad-njr <sajjadrahman1020@gmail.com>
2020-11-04 19:07:12 +0600

    float did not work mod

--- udemy-with-Abdul-Bari/28.variable-mention.cpp
+++ udemy-with-Abdul-Bari/28.variable-mention.cpp
@@ -1,1 +1,20 @@
+#include<iostream>
 
+using namespace std ;
+
+int main()
+{
+    //float o(9.0) ;
+    //float g( 8.98) ;
+
+    int  o(9.0) ;
+    int   g( 8.98) ;
+
+    //float c ;
+
+    int  c = o % g ;
+
+    cout << c ;
+    return 0 ;
+
+}
