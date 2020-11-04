0f3403aaeb433f7c4ca22ab5b5876369552b1100
sajjad-njr <sajjadrahman1020@gmail.com>
2020-11-04 19:11:04 +0600

    float did not work mod but int and char work

--- udemy-with-Abdul-Bari/28.variable-mention.cpp
+++ udemy-with-Abdul-Bari/28.variable-mention.cpp
@@ -4,15 +4,27 @@ using namespace std ;
 
 int main()
 {
-    //float o(9.0) ;
-    //float g( 8.98) ;
+    // float not work mood operation
 
+    /*float o(9.0) ;
+    float g( 8.98) ;
+      float   c = o % g ; */
+
+   // int work %
+   /*
     int  o(9.0) ;
     int   g( 8.98) ;
-
-    //float c ;
-
     int  c = o % g ;
+
+    */
+
+    // char work mood
+
+    char a(97);
+    char z(122);
+    //char c  = a % z;
+
+    int c = z % a ;
 
     cout << c ;
     return 0 ;
