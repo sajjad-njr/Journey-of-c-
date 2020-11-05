cbdce96bf2c67d29d55d904146d7a47149e6a7d4
sajjad-njr <sajjadrahman1020@gmail.com>
2020-11-06 01:22:41 +0600

    overflow char 127 is next -128

--- udemy-with-Abdul-Bari/40.bitwise-operator.cpp
+++ udemy-with-Abdul-Bari/40.bitwise-operator.cpp
@@ -7,12 +7,21 @@ int main()
     int a = 11 , b = 5 , c ;
 
     c = a & b ;
-    cout << "C = " << c << endl;
+    cout << "\nC = " << c << endl;
 
    c = a | b ;
-    cout << "C = " << c << endl;
+    cout << "\nC = " << c << endl;
 
     c = a ^ b ;
-    cout << "C = " << c << endl;
+    cout << "\nC = " << c << endl;
+
+    int x = 5 , y ;
+
+    y = x <<1;
+    cout << " \nY = " << y << endl;
+
+    y = ~x;
+    cout << " \nY = " << y << endl;
+
     return 0 ;
 }
