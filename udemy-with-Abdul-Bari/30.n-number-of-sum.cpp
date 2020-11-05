8b2142651b1c4ce13f359fd561325fc0d3f2f4dd
sajjad-njr <sajjadrahman1020@gmail.com>
2020-11-05 17:45:16 +0600

    N number sum

---
+++ udemy-with-Abdul-Bari/30.n-number-of-sum.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std ;
+
+int main()
+{
+    int  n ;
+     cout << "Enter a new number  N = ";
+     cin >> n ;
+     int s = n * (n + 1) / 2 ;
+
+     cout << "\n\n\tN number of sum = " << s << endl;
+
+     return 0 ;
+}
