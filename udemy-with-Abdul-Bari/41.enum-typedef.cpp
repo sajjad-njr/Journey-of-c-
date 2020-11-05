1b6ba581fc9afb57add84e146ea9c13e9b25a190
sajjad-njr <sajjadrahman1020@gmail.com>
2020-11-06 01:42:07 +0600

    enum typedef

---
+++ udemy-with-Abdul-Bari/41.enum-typedef.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std ;
+
+enum day {sun = 100 , mon , tue , wed , thu ,fri };
+
+int main()
+{
+    day d ;
+    //d = sun ;
+
+     d = mon ;
+
+    cout << d ;
+
+    return 0 ;
+}
