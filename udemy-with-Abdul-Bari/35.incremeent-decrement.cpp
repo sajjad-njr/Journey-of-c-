4fed16a030a5a5ae2689d8143414639beea306c8
sajjad-njr <sajjadrahman1020@gmail.com>
2020-11-05 23:37:55 +0600

    post and pre increement work

--- udemy-with-Abdul-Bari/35.incremeent-decrement.cpp
+++ udemy-with-Abdul-Bari/35.incremeent-decrement.cpp
@@ -4,13 +4,42 @@ using namespace std ;
 
 int main()
 {
-    int x = 9 , y ;
+    //step - 01
+
+   /* int x = 9 , y ;
 
     cout << " Before x  = " << x << endl;
     y = x++ ;
 
     cout << " Y = " << y << endl;
     cout << " X = " << x << endl;
+*/
+
+
+   //step 2
+    /*
+    int x = 9 , y , z = 10;
+
+    cout << " Before x , z  = " << x  << " " << z << endl;
+
+    y = x++ * z ;
+
+    cout << " Y = " << y << endl;
+    cout << " X = " << x << endl;
+    cout << " Z = " << z<< endl;
+*/
+
+    //step - 3  Pre increment
+
+    int x = 9 , y , z = 10;
+
+    cout << " Before x , z  = " << x  << " " << z << endl;
+
+    y = ++x * z ;
+
+    cout << " Y = " << y << endl;
+    cout << " X = " << x << endl;
+    cout << " Z = " << z<< endl;
 
     return 0 ;
 
