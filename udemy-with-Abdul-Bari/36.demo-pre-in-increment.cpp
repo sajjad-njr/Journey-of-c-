e38bfacc3d85a85aa36b8fb3f7247cddece0b06f
sajjad-njr <sajjadrahman1020@gmail.com>
2020-11-05 23:45:32 +0600

    more increement and decrement together

---
+++ udemy-with-Abdul-Bari/36.demo-pre-in-increment.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std ;
+
+int main()
+{
+    int a = 5 , b ;
+
+    b = 2 * a++ + 2 * a++ ;
+
+    cout << "b = " << b << " "<< " a = " << a<<  endl;
+
+    return 0 ;
+}
