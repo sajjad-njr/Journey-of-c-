8bb37ba2f3e461ce22ec09552efe996dbc01fa93
sajjad-njr <sajjadrahman1020@gmail.com>
2020-11-06 22:51:26 +0600

    compound condition

---
+++ udemy-with-Abdul-Bari/49.compound-condition-age.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std ;
+
+int main()
+{
+    int age ;
+
+    cin >> age ;
+
+    if ( age >> 16 && age << 29)
+    {
+        cout << "We are young man " << endl;
+
+    }
+    else
+    {
+        cout << "We are y your not allow for pogram " << endl;
+    }
+
+    return 0 ;
+}
