c95edd461eee9d21290fb2e224ea8c01c2d0aa8a
sajjad-njr <sajjadrahman1020@gmail.com>
2020-11-06 20:15:35 +0600

    validation of it

---
+++ udemy-with-Abdul-Bari/48.validation-of-condition.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+
+using namespace std ;
+
+
+int main()
+{
+    int c ;
+    cin >> c ;
+
+    if ( c < 0)
+    {
+        cout << "This is lower than = 0 " << endl;
+    }
+
+    else if ( c == 0)
+    {
+        cout << "This is  equal  than = 0 " << endl;
+    }
+
+    else
+    {
+        cout << "This is bigger  than = 0 " << endl;
+
+    }
+
+    return 0 ;
+}
