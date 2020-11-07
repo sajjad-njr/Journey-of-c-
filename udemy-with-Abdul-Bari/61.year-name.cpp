92c3759295812f9d2e3e0d830dd39cc685e6875d
sajjad-njr <sajjadrahman1020@gmail.com>
2020-11-07 22:35:23 +0600

    year name

---
+++ udemy-with-Abdul-Bari/61.year-name.cpp
@@ -0,0 +1,68 @@
+#include<iostream>
+
+using namespace std ;
+
+int main()
+{
+    int num ;
+
+    cin >> num;
+
+    if(num == 1)
+    {
+        cout << " The day is Sunday" << endl;
+    }
+
+    else if(num == 2)
+    {
+        cout << " The day is  Monday " << endl;
+    }
+    else if(num == 3)
+    {
+        cout << " The day is  Tuesday" << endl;
+    }
+    else if(num == 4)
+    {
+        cout << " The day is  Wednesday" << endl;
+    }
+    else if(num == 5)
+    {
+        cout << " The day is  Thursday" << endl;
+    }
+    else if(num == 6)
+    {
+        cout << " The day is Friday" << endl;
+    }
+    else if(num == 7)
+    {
+        cout << " The day is  Saturday" << endl;
+    }
+
+   else if(num == 8)
+    {
+        cout << " The day is  Wednesday" << endl;
+    }
+    else if(num == 9)
+    {
+        cout << " The day is  Thursday" << endl;
+    }
+    else if(num == 10)
+    {
+        cout << " The day is Friday" << endl;
+    }
+    else if(num == 11)
+    {
+        cout << " The day is  Saturday" << endl;
+    }
+     else if(num == 12)
+    {
+        cout << " The day is  Saturday" << endl;
+    }
+    else
+    {
+        cout << " wrong number bro" << endl;
+    }
+
+    return 0 ;
+}
+
