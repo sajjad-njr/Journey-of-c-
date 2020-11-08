4f5090b1a8b6d6c6815dfc51dd5bb3bcaa30deb4
sajjad-njr <sajjadrahman1020@gmail.com>
2020-11-08 19:29:39 +0600

    dynamic declare not work

---
+++ udemy-with-Abdul-Bari/62.daynamic-declaration.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std ;
+
+int main()
+{
+    int a = 9 ;
+    int b = 6;
+
+   if(int k < a )
+    {
+        cout << "This is work bro";
+    }
+
+    else
+    {
+        cout << " No work " ;
+    }
+
+    return 0 ;
+
+}
