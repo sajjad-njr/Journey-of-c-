ecb20389ae4e29c453d9bae555f64737fcc28e1f
sajjad-njr <sajjadrahman1020@gmail.com>
2020-11-09 23:23:15 +0600

    factorial missing

---
+++ udemy-with-Abdul-Bari/75.factorial.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std ;
+
+int main()
+{
+    int i, m, n = 5 , fact = 1;
+
+    for(i = 1 ; i <= n ; i++)
+    {
+        m =   fact * i  ;
+    }
+    cout << m ;
+
+    return 0 ;
+}
