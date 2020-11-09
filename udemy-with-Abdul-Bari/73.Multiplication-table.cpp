d3e4a5d61274a9d2e918236ed48374ef639fc92b
sajjad-njr <sajjadrahman1020@gmail.com>
2020-11-09 22:41:31 +0600

    multiplication table

---
+++ udemy-with-Abdul-Bari/73.Multiplication-table.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std ;
+
+int main()
+{
+    int i = 0 , n ;
+
+    cout << " Enter a number :" ;
+    cin >> n ;
+
+    for( i = 1 ; i <= 10 ; i++)
+    {
+        cout <<"\t" <<  n << " * " << i << "  =  " << n * i << endl;
+    }
+
+    return 0 ;
+}
