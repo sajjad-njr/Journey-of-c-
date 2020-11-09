739b183d42e24530f06a9e4743797a9f6dc0ebcd
sajjad-njr <sajjadrahman1020@gmail.com>
2020-11-09 23:17:21 +0600

    natural number add   using for loop

---
+++ udemy-with-Abdul-Bari/74.1.sum-of-natural-number.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std ;
+
+int main()
+{
+    int n, i = 1, sum = 0 ;
+
+    cout << " Enter a number = ";
+    cin >> n ;
+
+     while( i <= n)
+    {
+        cout << sum << " " << sum << "+" << i << endl;
+
+        sum = sum + i ;
+        i++;
+
+    }
+
+    cout << "\n\nTotal Natural number sum = " << sum << endl;
+
+    return 0 ;
+}
+
+
