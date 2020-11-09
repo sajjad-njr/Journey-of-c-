7cce166716d47fd8c69e118f73785b6ce1776299
sajjad-njr <sajjadrahman1020@gmail.com>
2020-11-09 17:53:51 +0600

    While test done

---
+++ udemy-with-Abdul-Bari/71.demo-loops.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std ;
+
+int main()
+{
+    int n , i ;
+
+    cout << " Enter a number : " ;
+    cin >> n ;
+
+    while (n)
+    {
+        cout << " Hello Sanju\n";
+        n--;
+    }
+
+    return 0 ;
+}
