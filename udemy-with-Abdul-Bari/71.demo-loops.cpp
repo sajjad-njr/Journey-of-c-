339edf2b9d083c7beca627c81bd99202a089d90b
sajjad-njr <sajjadrahman1020@gmail.com>
2020-11-09 18:07:21 +0600

    do while loop

--- udemy-with-Abdul-Bari/71.demo-loops.cpp
+++ udemy-with-Abdul-Bari/71.demo-loops.cpp
@@ -3,16 +3,24 @@ using namespace std ;
 
 int main()
 {
-    int n , i ;
+    int n , i = 0;
 
-    cout << " Enter a number : " ;
-    cin >> n ;
+    //cout << " Enter a number : " ;
+    //cin >> n ;
 
-    while (n)
+    /*while (n)
     {
         cout << " Hello Sanju\n";
         n--;
     }
+    */
+    do
+    {
+       cout << " Sajjad ";
+       cout << " \tDiya\n";
+       i++ ;
+    }
+    while(i < 10);
 
     return 0 ;
 }
