17b208699a360c1331b1da8ad406e999aed6ed20
sajjad-njr <sajjadrahman1020@gmail.com>
2020-11-11 23:13:48 +0600

    reverse

---
+++ udemy-with-Abdul-Bari/82.reverse-number.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std ;
+
+int main()
+{
+    int num , rem  , rev = 0 ;
+
+    cout << "Enter a number : " ;
+    cin >> num ;
+
+    while ( num > 0)
+    {
+        rem = num % 10 ;
+        num = num / 10 ;
+
+        rev = rev * 10 + rem ;
+
+    }
+
+    cout << " Rem = " << rem ;
+
+    return 0 ;
+}
