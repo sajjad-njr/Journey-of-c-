e6252a67cb51da4e4fc5e94b1c8b286805d10c55
sajjad-njr <sajjadrahman1020@gmail.com>
2020-11-11 23:43:22 +0600

    1002

--- OJ/uri/1002.cpp
+++ OJ/uri/1002.cpp
@@ -1,1 +1,15 @@
+#include<iostream>
+
+using namespace std ;
+#define n 3.14159
+
+int main()
+{
+     double R;
+     cin >> R ;
+     long double A = n * (R*R );
+
+     cout <<"A="<<A<<endl;
+     return 0 ;
+}
 
