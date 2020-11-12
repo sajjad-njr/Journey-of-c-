1c0a9c2fccc4e10771b2da2255c669139031a139
sajjad-njr <sajjadrahman1020@gmail.com>
2020-11-12 23:18:51 +0600

    c++ with array call and read donw

--- udemy-with-Abdul-Bari/self - try after BARI/liner.cpp
+++ udemy-with-Abdul-Bari/self - try after BARI/liner.cpp
@@ -10,10 +10,33 @@ int main()
         cin>>ar[i];
     }
 
+
+   int b , match = 0 , rep = 1 ;
+
+   cout << "Enter finding Number : " ;
+   cin >> b;
+
+
+
     for(i = 0 ; i < 10 ; i++)
     {
-         cout << " " << ar[i] << endl;
+        //cout << " " << ar[i] << endl;
+        if(ar[i] == b)
+        {
+            match = 1;
 
+            break;
+        }
+
+    }
+
+    if(match != 0)
+    {
+        cout << b << ' ' < "Data is found" << endl;
+    }
+    else
+    {
+         cout << b << ' ' <<"Data is not found" << endl;
     }
 
     return 0 ;
