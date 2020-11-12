c9b7cadde381eae2930e3b6eea1768738cce2af6
sajjad-njr <sajjadrahman1020@gmail.com>
2020-11-12 23:13:13 +0600

    c++ with array call and read donw

---
+++ udemy-with-Abdul-Bari/self - try after BARI/liner.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std ;
+
+int main()
+{
+    int ar[10] , i ;
+
+    for(i = 0 ; i < 10 ; i++)
+    {
+        cin>>ar[i];
+    }
+
+    for(i = 0 ; i < 10 ; i++)
+    {
+         cout << " " << ar[i] << endl;
+
+    }
+
+    return 0 ;
+
+}
