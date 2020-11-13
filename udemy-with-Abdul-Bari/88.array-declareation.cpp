445e93567b187f3990e6fe73fb17eb8dc198966b
sajjad-njr <sajjadrahman1020@gmail.com>
2020-11-13 20:27:58 +0600

    array declareation  and show the auto different

--- udemy-with-Abdul-Bari/88.array-declareation.cpp
+++ udemy-with-Abdul-Bari/88.array-declareation.cpp
@@ -4,9 +4,11 @@ using namespace std ;
 
 int main()
 {
-      float arr[] = {45.4f,67.8f,89.3f,43.09f,23.99f};
+      //float arr[] = {45.4f,67.8f,89.3f,43.09f,23.99f};
 
-    for(float x:arr)
+   char arr[] = {'A',78,65,'B','C'};
+
+    for(int  x:arr)
         cout << x <<endl;
 
 
