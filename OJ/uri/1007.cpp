1031d6434067d752dfb1b488a23575ce2869ddd0
sajjad-njr <sajjadrahman1020@gmail.com>
2020-11-13 23:27:08 +0600

    1007 1sr chance done

---
+++ OJ/uri/1007.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std ;
+
+int main()
+{
+    int  A,B,C,D;
+
+    cin >> A;
+    cin >> B;
+    cin >> C;
+    cin >> D;
+
+    int d ;
+    d = (A*B-C*D);
+    cout <<"DIFERENCA" << " = " <<d << endl;
+    return 0 ;
+}
