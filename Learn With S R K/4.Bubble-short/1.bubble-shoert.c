277d5b9bb4727793f869ae0003a21ae89179f2bc
sajjad-njr <sajjadrahman1020@gmail.com>
2020-11-14 19:49:18 +0600

    bubble short

--- Learn With S R K/4.Bubble-short/1.bubble-shoert.c
+++ Learn With S R K/4.Bubble-short/1.bubble-shoert.c
@@ -1,1 +1,38 @@
+#include<stdio.h>
 
+void main()
+{
+    int N , i , j , temp;
+    printf("Enter a Array size : ");
+    scanf("%d",&N);
+
+    int A[N];
+    printf("\n\tEnter Array value : ");
+    for(i = 0 ; i < N ; i++)
+    {
+        scanf("%d",&A[i]);
+    }
+
+
+    /*Shorting Part*/
+
+    for(i = 0 ; i < N-1 ; i++)
+    {
+        for(j = 0 ; j < N - 1- i ; j++)
+        {
+            if(A[j] > A[j+1])
+            {
+                temp = A[j];
+                A[j] = A[j+1];
+                A[j+1] = temp ;
+            }
+        }
+    }
+
+    printf("\n\n\t\tAfter Shorting Value : ");
+
+    for(i = 0 ; i < N ; i++)
+    {
+         printf(" %d ",A[i]);
+    }
+}
