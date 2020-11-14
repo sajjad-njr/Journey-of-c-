18b1c57bdc03b4891a505814b96e41f207111ddd
sajjad-njr <sajjadrahman1020@gmail.com>
2020-11-14 20:48:43 +0600

    function  bubble short

--- Learn With S R K/4.Bubble-short/2.function-with-buble-short.c
+++ Learn With S R K/4.Bubble-short/2.function-with-buble-short.c
@@ -1,8 +1,9 @@
 #include<stdio.h>
+void BubbleShort(int A[],int N);
 
 void main()
 {
-    int N , i , j , temp;
+    int N, i, j, temp;
     printf("Enter a Array size : ");
     scanf("%d",&N);
 
@@ -13,12 +14,33 @@ void main()
         scanf("%d",&A[i]);
     }
 
+    BubbleShort(A,N);
 
     printf("\n\n\t\tAfter Shorting Value : ");
 
     for(i = 0 ; i < N ; i++)
     {
-         printf(" %d ",A[i]);
+        printf(" %d ",A[i]);
     }
 }
 
+void BubbleShort(int A[],int N)
+{
+    /*Shorting Part*/
+    int i, j, temp;
+
+    for(i = 0 ; i < N-1 ; i++)
+    {
+        for(j = 0 ; j < N - 1- i ; j++)
+        {
+            if(A[j] > A[j+1])
+            {
+                temp = A[j];
+                A[j] = A[j+1];
+                A[j+1] = temp ;
+            }
+        }
+    }
+
+
+}
