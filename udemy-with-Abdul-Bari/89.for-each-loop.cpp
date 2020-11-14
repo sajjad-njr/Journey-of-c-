c2686faa7d5d804a4251c7a4933f0e0bab80966d
sajjad-njr <sajjadrahman1020@gmail.com>
2020-11-14 21:53:00 +0600

    for each loop

--- udemy-with-Abdul-Bari/89.for-each-loop.cpp
+++ udemy-with-Abdul-Bari/89.for-each-loop.cpp
@@ -6,9 +6,15 @@ int main()
 {
     int Ar[] = {2,3,5,6,7,8};
 
+    /*
     for(int i = 0 ; i < 6 ; i++)
     {
-        cout << Ar[i];
+        cout << " " <<Ar[i];
+    }
+    */
+    for(int x : Ar)
+    {
+        cout << " " << x ;
     }
     return 0;
 }
