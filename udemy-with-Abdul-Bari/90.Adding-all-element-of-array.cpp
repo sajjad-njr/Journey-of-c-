a69493f555a78022567222d89946846a200af4e8
sajjad-njr <sajjadrahman1020@gmail.com>
2020-11-14 21:58:25 +0600

    adding all element of array

---
+++ udemy-with-Abdul-Bari/90.Adding-all-element-of-array.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int A[] = {1 , 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+    int i , sum = 0 ;
+
+    for(i = 0 ; i < 10 ; i++)
+    {
+        sum = sum + A[i];
+    }
+
+    cout << "Total sum = " << sum << endl;
+
+    return 0 ;
+}
