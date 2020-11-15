3aec5c3bf28491019e673706aa711c289630da91
sajjad-njr <sajjadrahman1020@gmail.com>
2020-11-15 23:33:51 +0600

    1006 salary

--- OJ/uri/1008.cpp
+++ OJ/uri/1008.cpp
@@ -1,1 +1,19 @@
+#include<iostream>
+#include<iomanip>
+using namespace std ;
 
+int main()
+{
+    int a, b ;
+    double c,d;
+
+    cin >> a;
+    cin>>b;
+    cin >> c ;
+
+    d = b * c ;
+    cout << "NUMBER" << " = " << a <<endl;
+    cout << "SALARY" << " = " <<"U$ " <<fixed << setprecision(2) << d << endl;
+
+    return 0;
+}
