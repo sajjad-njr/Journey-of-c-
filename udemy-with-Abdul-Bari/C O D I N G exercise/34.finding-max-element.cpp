aabb7c140e273bdeb34a0c04b339426fdda42bd4
sajjad-njr <sajjadrahman1020@gmail.com>
2020-11-15 13:03:40 +0600

    cd 34  maximum value

--- udemy-with-Abdul-Bari/C O D I N G exercise/34.finding-max-element.cpp
+++ udemy-with-Abdul-Bari/C O D I N G exercise/34.finding-max-element.cpp
@@ -6,6 +6,7 @@ void Max(int a[])
     // int a[5] = { 89 , 99, 65, 43, 23};
     int i, max = a[0] ;
 
+   // write  a loop find max value
     for( i = 0 ; i < 5 ; i++)
     {
         if(a[i] > max)
@@ -20,7 +21,7 @@ void Max(int a[])
 
 int main()
 {
-    int A[5] = { 89, 99, 65, 463, 23};
+    int A[5] = { 89, 99, 65, 463, 23453};
     //int i, max = a[0] ;
 
     Max(A);
