8eaf189d84161226d29e6c120e75eb4321b1445e
sajjad-njr <sajjadrahman1020@gmail.com>
2020-11-16 19:53:37 +0600

    int-min

---
+++ udemy-with-Abdul-Bari/94.int-min-max.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std ;
+
+int main()
+{
+    int a[10] = {6,7,8,9,5,3,19,7,8,9};
+    int i ;
+
+    /*
+    int max = INT_MIN ;
+
+    for(auto x: a)
+    {
+        if(x > max)
+            max = x;
+    }
+    cout << max << endl;
+    */
+
+    return 0 ;
+}
+
