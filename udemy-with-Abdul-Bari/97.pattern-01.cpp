4a0f1140043c80fb236bab4e4f9f2c97918fd251
sajjad-njr <sajjadrahman1020@gmail.com>
2020-11-16 21:35:51 +0600

    pattern - 1 (1 - 16) all value by 4 lines

---
+++ udemy-with-Abdul-Bari/97.pattern-01.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std ;
+
+int main()
+{
+    int i,j,count = 1 ;
+
+    for(i = 0 ; i < 4 ; i++)
+    {
+        for(j = 0 ; j < 4 ; j++)
+        {
+            cout << count++<< "  ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
