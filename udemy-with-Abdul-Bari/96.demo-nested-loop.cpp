ced26306743f8432b8c3ac7a82c18cf7d4be4581
sajjad-njr <sajjadrahman1020@gmail.com>
2020-11-16 21:31:29 +0600

    nested loop  with value () in backet

---
+++ udemy-with-Abdul-Bari/96.demo-nested-loop.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std ;
+
+int main()
+{
+    int i ,j ;
+
+    for ( i = 0 ; i < 5 ; i++)
+    {
+        for(j = 0 ; j < 5 ; j++)
+        {
+            cout << " (" << i << "," << j << ") " ;
+        }
+        cout << endl;
+    }
+}
