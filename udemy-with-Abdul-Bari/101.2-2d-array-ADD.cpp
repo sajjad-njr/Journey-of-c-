59b50b10bfa850299a2e013e634c533f229871dd
sajjad-njr <sajjadrahman1020@gmail.com>
2020-11-19 22:34:24 +0600

    2d array   adding

---
+++ udemy-with-Abdul-Bari/101.2-2d-array-ADD.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std ;
+
+int main()
+{
+    int i , j , m , n;
+    cout <<"Here M and N is the row and column of a Matrix" << endl;
+
+    cin >> m >> n ;
+    int a[m][n];
+    int b[m][n];
+    int c[m][n];
+
+}
