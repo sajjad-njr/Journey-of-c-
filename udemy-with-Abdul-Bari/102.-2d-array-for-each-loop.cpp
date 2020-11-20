5aee658fcbfca8f0a3c99a16277ad13b33090154
sajjad-njr <sajjadrahman1020@gmail.com>
2020-11-20 16:23:52 +0600

    for each loop to 2D array

--- udemy-with-Abdul-Bari/102.-2d-array-for-each-loop.cpp
+++ udemy-with-Abdul-Bari/102.-2d-array-for-each-loop.cpp
@@ -3,6 +3,7 @@ using namespace std;
 int main()
 {
     int a[2][3];
+    int sum = 0 ;
 
     for(auto& x : a)
     {
@@ -17,7 +18,20 @@ int main()
         for(auto& y : x)
         {
             cout << y << " ";
+            sum += y ;
         }
         cout << endl;
     }
+
+    //adding part
+   /* for(auto& x : a)
+    {
+        for(auto& y : x)
+        {
+            cout << y << " ";
+        }
+        cout << endl;
+    } */
+    cout << endl << " SUM = " << sum << endl;
+
 }
