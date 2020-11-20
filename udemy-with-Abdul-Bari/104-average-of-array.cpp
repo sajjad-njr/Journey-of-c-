b87bde29b697406809b7ab754f2dd3b469db04df
sajjad-njr <sajjadrahman1020@gmail.com>
2020-11-20 16:30:28 +0600

    average of sum

--- udemy-with-Abdul-Bari/104-average-of-array.cpp
+++ udemy-with-Abdul-Bari/104-average-of-array.cpp
@@ -3,5 +3,17 @@ using namespace std ;
 
 int main()
 {
+    float num[100] , sum = 0.0 , average;
+    int n ;
+    cout <<"Enter the numbers of the array : " ;
+    cin >> n;
+
+    for(i = 0 ; i < n ; i++)
+    {
+        cout << i+1 << "Enter number : " ;
+        cin >> num[i];
+
+        sum += num[i];
+    }
 
 }
