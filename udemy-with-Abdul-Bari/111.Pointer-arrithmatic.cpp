19a7b24742ecb5e2010705d39ead5042e805f003
sajjad-njr <sajjadrahman1020@gmail.com>
2020-11-21 22:41:29 +0600

    substrac 2 pointer by two different array

--- udemy-with-Abdul-Bari/111.Pointer-arrithmatic.cpp
+++ udemy-with-Abdul-Bari/111.Pointer-arrithmatic.cpp
@@ -4,8 +4,16 @@ using namespace std;
 
 int main()
 {
-    int a[5] = { 4 , 6, 7, 8, 9};
-    int *p = a;
+    int a[5] = { 4, 6, 7, 8, 9};
+    int b[5] = {22,34,56,78,90};
 
-    cout << *p;
+    int *q = &a[2];
+    int *p = &b[4];
+
+    int d = q  - p ;
+
+
+
+    cout << d;
+    return 0;
 }
