894afcf8e80f8ece6a9469530877d153dc812cef
sajjad-njr <sajjadrahman1020@gmail.com>
2020-11-22 21:44:02 +0600

    for loop add

--- udemy-with-Abdul-Bari/112.pointer-demo.cpp
+++ udemy-with-Abdul-Bari/112.pointer-demo.cpp
@@ -6,9 +6,15 @@ int main()
     int A[]{2,4,5,6,8,9,80};
 
     int *p = A ;
-
+/*
     cout << *p << endl;
     p++ ;
     cout << *p << endl;
+*/
+    for(int i = 0 ; i < 7; i++)
+    {
+       // cout << A[i] << endl;
+    }
+
     return 0;
 }
