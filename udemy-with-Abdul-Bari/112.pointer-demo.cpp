7697a8325ce91ef128722ceb2198002da2283f03
sajjad-njr <sajjadrahman1020@gmail.com>
2020-11-22 21:39:12 +0600

    pointer demo

--- udemy-with-Abdul-Bari/112.pointer-demo.cpp
+++ udemy-with-Abdul-Bari/112.pointer-demo.cpp
@@ -3,10 +3,12 @@ using namespace std;
 
 int main()
 {
-    int A[5]{2,4,5,6,8};
+    int A[]{2,4,5,6,8,9,80};
 
     int *p = A ;
 
+    cout << *p << endl;
+    p++ ;
     cout << *p << endl;
     return 0;
 }
