89bc34671d4617075c88c080f92e1da2a2f6f59d
sajjad-njr <sajjadrahman1020@gmail.com>
2020-11-23 22:20:56 +0600

    1010 done

--- OJ/uri/1010.cpp
+++ OJ/uri/1010.cpp
@@ -1,16 +1,15 @@
 #include<bits/stdc++.h>
-//#include<iostream>
 using namespace std;
-
 int main()
 {
-    int a , b ;
-    double c , sum;
-
-    cin >> a >> b ;
-    sum = a + b;
-    cout << sum;
-
+    int a, b ;
+    double c, sum = 0.0;
+    for(int i = 0 ; i < 2 ; i++)
+    {
+        cin >> a >> b >> c;
+        cout << endl;
+        sum += b * c ;
+    }
+    cout << "VALOR A PAGAR: R$ "<<fixed << setprecision(2) << sum << endl;
     return 0;
-
 }
