a5a2ae1e6a15a969463d6b5c895480c7c639ba28
sajjad-njr <sajjadrahman1020@gmail.com>
2020-11-23 22:29:47 +0600

    finally accepted  1010 done

--- OJ/uri/1010.cpp
+++ OJ/uri/1010.cpp
@@ -3,11 +3,11 @@ using namespace std;
 int main()
 {
     int a, b ;
-    double c, sum = 0.0;
+    double c, sum = 0.00;
     for(int i = 0 ; i < 2 ; i++)
     {
         cin >> a >> b >> c;
-        cout << endl;
+        //cout << endl;
         sum += b * c ;
     }
     cout << "VALOR A PAGAR: R$ "<<fixed << setprecision(2) << sum << endl;
