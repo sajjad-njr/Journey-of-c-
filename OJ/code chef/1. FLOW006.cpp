0d02257865bcc82403ac891fdf6aa6b9002e31c2
sajjad-njr <sajjadrahman1020@gmail.com>
2020-11-24 20:44:31 +0600

    sum of all digit

--- OJ/code chef/1. FLOW006.cpp
+++ OJ/code chef/1. FLOW006.cpp
@@ -1,1 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int T , N ;
+    cin>>T>>N;
 
+    for(int i = 0 ; i < T ; i++)
+    {
+        cout << "sajjad" << endl;
+    }
+    return 0;
+}
