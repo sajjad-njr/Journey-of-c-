0f63666dcbd4b0bea025f58fc9642298cac3eb4b
sajjad-njr <sajjadrahman1020@gmail.com>
2020-11-24 21:16:40 +0600

    sum of all digit

--- OJ/code chef/1. FLOW006.cpp
+++ OJ/code chef/1. FLOW006.cpp
@@ -2,18 +2,18 @@
 using namespace std;
 int main()
 {
-    int T , N , sum = 0 ;
-    cin>>T >> N;
-
+    int T, N, sum;
+    cin>>T;
     while(T--)
     {
-         for(int j = 0 ; j < N ; j++)
-         {
-             cin >> j;
-             sum += j;
-         }
-         cout << "Sum = " << sum << endl;
+        cin >> N;
+        sum = 0 ;
+        while(N>0)
+        {
+            sum += (N%10);
+            N = N / 10;
+        }
+        cout << sum << endl;
     }
-
     return 0;
 }
