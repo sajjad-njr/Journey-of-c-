2b1c698b125ebbc4c390ac37b6da7212d3aa8e54
sajjad-njr <sajjadrahman1020@gmail.com>
2020-11-24 22:51:54 +0600

    two degit

---
+++ OJ/code chef/2.FLOW001.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a , b , c ,add;
+    cin>>a;
+    for(int i = 0 ; i < a ; i++)
+    {
+         cin >> b >> c;
+         add = b + c;
+         cout << add << endl;
+    }
+  return 0;
+}
