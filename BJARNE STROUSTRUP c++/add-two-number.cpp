60ce1e7783b3bb68b739681faa76b9cf539a77ea
sajjad-njr <sajjadrahman1020@gmail.com>
2020-11-24 23:18:23 +0600

    functionaly done

--- BJARNE STROUSTRUP c++/add-two-number.cpp
+++ BJARNE STROUSTRUP c++/add-two-number.cpp
@@ -1,7 +1,22 @@
 #include<iostream>
 using namespace std;
 
-int main()
+int add(int i , int j)
 {
 
+    return i + j ;
+}
+
+int main()
+{
+    int a , b , c , d ;
+    cin >> a;
+    for(int i = 0 ; i < a ; i++ )
+    {
+        cin >> b >> c;
+        d = add(b,c);
+        cout <<  d << endl;
+    }
+
+    return 0;
 }
