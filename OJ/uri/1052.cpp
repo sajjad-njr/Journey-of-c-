85f840ac50f1865317673e6d37e97266930e22e1
sajjad-njr <sajjadrahman1020@gmail.com>
2020-11-26 00:46:18 +0600

    1052 done

--- OJ/uri/1052.cpp
+++ OJ/uri/1052.cpp
@@ -5,46 +5,31 @@ int main()
 {
     int n ;
     cin>>n;
-    switch(n)
-    {
-    case 1 :
-        cout <<"January";
-        break;
-    case 2 :
-        cout <<"February";
-        break;
-    case 3 :
-        cout <<"March";
-        break;
-    case 4 :
-        cout <<"April";
-        break;
-    case 5 :
-        cout <<"May";
-        break;
-    case 6 :
-        cout <<"June";
-        break;
-    case 7 :
-        cout <<"July";
-        break;
-    case 8 :
-        cout <<"August";
-        break;
-    case 9 :
-        cout <<"September";
-        break;
-    case 10 :
-        cout <<"October";
-        break;
-    case 11 :
-        cout <<"November";
-        break;
-    case 12 :
-        cout <<"December";
-        break;
 
-    }
+    if(n==1)
+        cout <<"January";
+    if(n==2)
+        cout <<"February";
+    if(n==3)
+        cout <<"March";
+    if(n==4)
+        cout <<"April";
+    if(n==5)
+        cout <<"May";
+    if(n==6)
+        cout <<"June";
+    if(n==7)
+        cout <<"July";
+    if(n==8)
+        cout <<"August";
+    if(n==9)
+        cout <<"September";
+    if(n==10)
+        cout <<"October";
+    if(n==11)
+        cout <<"November";
+    if(n==12)
+        cout <<"December";
 
     return 0;
 }
