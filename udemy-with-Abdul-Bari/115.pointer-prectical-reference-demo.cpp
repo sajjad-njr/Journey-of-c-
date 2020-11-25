82a05aeedc04425dfd63eedc5ee109e549e9954c
sajjad-njr <sajjadrahman1020@gmail.com>
2020-11-25 19:33:37 +0600

    reference

--- udemy-with-Abdul-Bari/115.pointer-prectical-reference-demo.cpp
+++ udemy-with-Abdul-Bari/115.pointer-prectical-reference-demo.cpp
@@ -1,1 +1,15 @@
+#include<iostream>
+using namespace std;
 
+int main()
+{
+    int x = 10 ;
+    int &y = x;
+    cout << "X = " << x << endl;
+    y++;
+    x++;
+
+    cout << "X = " << x << endl;
+
+    return 0;
+}
