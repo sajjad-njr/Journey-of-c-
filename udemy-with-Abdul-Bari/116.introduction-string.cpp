b716dd440c48017fc7112003a3e09b6693f31aa2
sajjad-njr <sajjadrahman1020@gmail.com>
2020-11-25 20:01:00 +0600

    stinrg introduction

--- udemy-with-Abdul-Bari/116.introduction-string.cpp
+++ udemy-with-Abdul-Bari/116.introduction-string.cpp
@@ -7,12 +7,15 @@ int main()
     //char s[10] = "hello";
 
     //  2nd step
-    //char s[] = "hello";
+    char s[] = "hello diya";
 
     //3rd step
     //char s[] = {'H','E','L','L','O','\0','a','a','f'};
 
-    char s[] = {67,68,69,69,78,90,87,'\0'};
+    //char s[] = {67,68,69,69,78,90,87,'\0'};
+
+    //char  *s = "Hello  DIYA ";
+    //string s = "hello  diya ";
     cout << s << endl;
 
     return 0;
