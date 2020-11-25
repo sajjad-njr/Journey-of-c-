6fdffe3c00748c10fd2c01206fc047ecb9d61636
sajjad-njr <sajjadrahman1020@gmail.com>
2020-11-25 19:57:43 +0600

    stinrg introduction

--- udemy-with-Abdul-Bari/116.introduction-string.cpp
+++ udemy-with-Abdul-Bari/116.introduction-string.cpp
@@ -6,12 +6,13 @@ int main()
     // 1st step
     //char s[10] = "hello";
 
-     //  2nd step
+    //  2nd step
     //char s[] = "hello";
 
-        //3rd step
+    //3rd step
     //char s[] = {'H','E','L','L','O','\0','a','a','f'};
 
+    char s[] = {67,68,69,69,78,90,87,'\0'};
     cout << s << endl;
 
     return 0;
