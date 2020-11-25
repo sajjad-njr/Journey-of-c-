6791d4b332afcd94587510e3d8e33683aaa013f3
sajjad-njr <sajjadrahman1020@gmail.com>
2020-11-25 21:49:39 +0600

    struct pogram

---
+++ BJARNE STROUSTRUP c++/tasmi.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+struct Student
+{
+
+    string name, address;
+    int id;
+    float mark;
+};
+
+int main()
+{
+    Student info[5];
+    int m;
+    cin >> m;
+
+
+        for(int i=0; i<2; i++)
+        {
+            getline(cin,info[i].name);
+            cin>>info[i].id;
+            cin>>info[i].address;
+            cin>>info[i].mark;
+            getchar();
+
+        }
+
+    for(int i=0; i<2; i++)
+    {
+        cout<<info[i].name<<" "<<info[i].id<<" "<<info[i].address<<" "<<info[i].mark<<endl;
+    }
+
+    return 0;
+
+}
+
