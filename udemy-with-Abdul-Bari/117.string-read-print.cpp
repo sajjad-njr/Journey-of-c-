29472f7d31229c7ba73eaba19e4e1db91885016f
sajjad-njr <sajjadrahman1020@gmail.com>
2020-11-26 21:09:15 +0600

    string read and print

--- udemy-with-Abdul-Bari/117.string-read-print.cpp
+++ udemy-with-Abdul-Bari/117.string-read-print.cpp
@@ -1,6 +1,11 @@
-#include<bits/sdtc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 int main()
 {
+    char s[20];
+    cout <<"Enter your name : ";
+    cin >> s ;
 
+    cout << s;
+    return 0;
 }
