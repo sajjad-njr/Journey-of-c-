c9619522c638d0b5a3b1a412b020b87ce7edff64
sajjad-njr <sajjadrahman1020@gmail.com>
2020-11-26 20:51:52 +0600

    dev bishal

---
+++ Data Structure - 3G/Dev - Bisal/LAB/1.stirng.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+#include<string>
+using namespace std;
+
+int main()
+{
+    string str1[100],str2[100];
+    int i;
+
+    gets(str1);
+    gets(str2);
+
+    puts(str1);
+    puts(str2);
+}
