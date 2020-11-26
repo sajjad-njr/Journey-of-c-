7036904441b2dec184b5ca5df706cfe94ba12fed
sajjad-njr <sajjadrahman1020@gmail.com>
2020-11-26 21:11:50 +0600

    string read and print

--- udemy-with-Abdul-Bari/117.string-read-print.cpp
+++ udemy-with-Abdul-Bari/117.string-read-print.cpp
@@ -2,10 +2,19 @@
 using namespace std;
 int main()
 {
+    //single line means before catch space its return ;
+    /*
     char s[20];
     cout <<"Enter your name : ";
     cin >> s ;
-
     cout << s;
+    */
+
+    // double name print
+    char s[20];
+    cout <<"Enter your name : ";
+    cin.get(s,20);
+    cout << s ;
+
     return 0;
 }
