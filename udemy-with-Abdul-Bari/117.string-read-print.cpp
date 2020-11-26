8c709fdd94d7353309f919d75f5144ea9c43d1b9
sajjad-njr <sajjadrahman1020@gmail.com>
2020-11-26 21:36:04 +0600

    string read and print

--- udemy-with-Abdul-Bari/117.string-read-print.cpp
+++ udemy-with-Abdul-Bari/117.string-read-print.cpp
@@ -2,6 +2,23 @@
 using namespace std;
 int main()
 {
+
+
+    char s[20];
+    char s2[20];
+
+    cout <<"Enter your name : ";
+    cin.get(s,20);
+
+    // use multiple line otherwise could not work
+    cin.ignore();
+
+    cout <<"Enter your name : ";
+    cin.get(s2,20);
+
+    cout << s << endl;
+    cout << s2 <<endl ;
+
     //single line means before catch space its return ;
     /*
     char s[20];
@@ -25,7 +42,7 @@ int main()
     */
 
 //the deference between get line
-
+    /*
     char s[20];
     char s2[20];
     cout <<"Enter your name : ";
@@ -37,5 +54,7 @@ int main()
     cin.getline(s2,20);
     cout << s2 ;
     cout << endl;
+
+    */
     return 0;
 }
