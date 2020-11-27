dfb985072b1db75b6da9f4cdc99000e873080869
sajjad-njr <sajjadrahman1020@gmail.com>
2020-11-27 18:35:57 +0600

    string read and print

---
+++ S T R I N G/sanjay gupta/1.read-and-print-string.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+    char str[10];
+    printf("Enter your name : ");
+
+    gets(str);
+
+    printf("\nEnter your name : ");
+
+    puts(str);
+
+    return 0;
+}
