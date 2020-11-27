8b091a2b6cd83459c75dd810ef22d6eaa84623c3
sajjad-njr <sajjadrahman1020@gmail.com>
2020-11-27 21:33:44 +0600

    string cancatenation

--- S T R I N G/sanjay gupta/10.0.string-cancatenation.c
+++ S T R I N G/sanjay gupta/10.0.string-cancatenation.c
@@ -1,6 +1,29 @@
 #include<stdio.h>
 
-int main()
+void main()
 {
     char s1[20],s2[20];
+
+    int i = 0 , c = 0 ;
+    printf("\nEnter 1st line : ");
+    gets(s1);
+
+    printf("\nEnter 2nd line : ");
+    gets(s2);
+
+    for(i = 0 ; s1[i] !='\0' ; i++)
+        c++;
+
+    s1[c] = ' ';
+    c++;
+
+    for(i = 0 ; s2[i] != '\0' ; i++)
+    {
+        s1[c] = s2[i];
+        c++;
+    }
+    s1[c] = '\0';
+
+    printf("Cat string = %s",s1);
+
 }
