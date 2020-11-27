7185da6fbdc2e8447551f95df417f08dc67c8f93
sajjad-njr <sajjadrahman1020@gmail.com>
2020-11-27 19:07:30 +0600

    upper-lower-number-special-count

--- S T R I N G/sanjay gupta/3.upper-lower-number-special-COUNT.c
+++ S T R I N G/sanjay gupta/3.upper-lower-number-special-COUNT.c
@@ -2,10 +2,20 @@
 
 int main()
 {
-    int i ;
+    int i CountUp = CountLow = CountNum = CounSp = 0 ;
     char str[100];
+
     printf("\nEnter a line with mix : ");
     gets(str);
 
+    for(i = 0 ; str[i] != '\0' ; i++)
+    {
+        if(str[i] >= 'A' && str[i] <= 'Z')
+            CountUp++;
+        else
+            if(str[i] >= 'a' && str[i] <= 'z')
+                CountLow++;
+            else
 
+    }
 }
