7eb7e3a820f4506b7e3d9ce509d73f7fa0d2371f
sajjad-njr <sajjadrahman1020@gmail.com>
2020-11-29 00:00:58 +0600

    str cmp

--- udemy-with-Abdul-Bari/120.1-string-cmp.cpp
+++ udemy-with-Abdul-Bari/120.1-string-cmp.cpp
@@ -4,8 +4,14 @@ int main()
 {
     char s1[20], s2[20];
 
-    cin.getline(s1);
-    cin.getline(s2);
+    cin.getline(s1,20);
+    cin.getline(s2,20);
 
-    cout << strcmp(s1,s2);
+    if(strcmp(s1,s2) == 0)
+        cout<<"both are equal" << endl;
+    else
+         cout<<"no same " << endl;
+
+
+    return 0;
 }
