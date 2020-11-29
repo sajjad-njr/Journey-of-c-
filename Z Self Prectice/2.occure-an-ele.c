49b09571cf534a3bf8e2c0be10d02544934a9020
sajjad-njr <sajjadrahman1020@gmail.com>
2020-11-29 15:19:16 +0600

    occure an element

--- Z Self Prectice/2.occure-an-ele.c
+++ Z Self Prectice/2.occure-an-ele.c
@@ -1,14 +1,18 @@
 #include<stdio.h>
 int main()
 {
-    int ar[10], i ;
+    int ar[10], i item = 0;
     for(i = 0 ; i < 10 ; i++)
     {
         scanf("%d",&ar[i]);
     }
 
+    printf("\nsearch item = ");
+    scanf("%d",&item);
+
     for(i = 0 ; i < 10 ; i++)
     {
-
+         printf(" %d ",ar[i]);
     }
+    return 0;
 }
