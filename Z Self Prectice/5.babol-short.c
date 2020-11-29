532fd93e64d1509c3583d36722fd14d7d094b2b2
sajjad-njr <sajjadrahman1020@gmail.com>
2020-11-29 22:45:31 +0600

    bubble sort

--- Z Self Prectice/5.babol-short.c
+++ Z Self Prectice/5.babol-short.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main()
 {
-  int ar[5], i ;
+    int ar[5], i, j, temp ;
 
     for(i = 0 ; i < 5 ; i++)
     {
@@ -9,6 +9,26 @@ int main()
     }
 
     printf("\n\t Before bubble sort  =  ");
+
+    for(i = 0 ; i < 5 ; i++)
+    {
+        printf(" %d ",ar[i]);
+
+    }
+
+    for(i = 0 ; i < 5 ; i++)
+    {
+        for( j = 0 ; j < 5 - i - 1 ; j++)
+        {
+            if(ar[j] > ar[j+1])
+            {
+                temp = ar[j];
+                ar[j] = ar[j+1];
+                ar[j+1] = temp;
+            }
+        }
+    }
+    printf("\n\t After  bubble sort  =  ");
 
     for(i = 0 ; i < 5 ; i++)
     {
