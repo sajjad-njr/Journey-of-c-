703f2cdce10902e0095e1ff0398ced5b2813141a
sajjad-njr <sajjadrahman1020@gmail.com>
2020-11-29 23:25:49 +0600

    PPSUM

--- OJ/code chef/6.PPSUM.cpp
+++ OJ/code chef/6.PPSUM.cpp
@@ -3,13 +3,21 @@ using namespace std ;
 
 int main()
 {
-    int n , d , m , x ;
+    int n, i , d, m, x;
     cin>> n;
 
     while(n--)
     {
         cin >> d >> m ;
         x = d * m;
+        int sum = 0;
+        for(i = 1  ; i <= x ; i++ )
+        {
+            sum = sum + i ;
 
+        }
+          cout << sum << endl;
     }
+
+    return 0;
 }
