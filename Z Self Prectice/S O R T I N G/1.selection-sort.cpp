cfda95183283715888653a86d7b84cab17f4ab28
sajjad-njr <sajjadrahman1020@gmail.com>
2020-11-30 21:40:44 +0600

    selection short

---
+++ Z Self Prectice/S O R T I N G/1.selection-sort.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std ;
+
+void SelelectionSort(int a[], int n )
+{
+    for(int i = 0 ; i < n - 1 ; i++)
+    {
+        int imin = i ;
+
+        for(int j = 0 ; j < n ; j++)
+        {
+
+        }
+
+    }
+}
