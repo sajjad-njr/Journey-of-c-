d87d9b52a28db853528fe4dd4c8ffa174251ad5b
sajjad-njr <sajjadrahman1020@gmail.com>
2020-11-30 22:19:46 +0600

    selection short

--- Z Self Prectice/S O R T I N G/1.selection-sort.cpp
+++ Z Self Prectice/S O R T I N G/1.selection-sort.cpp
@@ -3,11 +3,12 @@ using namespace std ;
 
 void SelelectionSort(int a[], int n )
 {
-    for(int i = 0 ; i < n - 1 ; i++)
+   int i , j ;
+    for(  i = 0 ; i < n - 1 ; i++)
     {
         int imin = i ;
 
-        for(int j = 0 ; j < n ; j++)
+        for( j = i+1 ; j < n ; j++)
         {
             if(a[j] < a[imin])
                 imin = j ;
