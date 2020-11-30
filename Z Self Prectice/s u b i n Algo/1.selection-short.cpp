239405e4a94ef8f797b9280170a1c2660832472a
sajjad-njr <sajjadrahman1020@gmail.com>
2020-11-30 22:09:37 +0600

    selection short

---
+++ Z Self Prectice/s u b i n Algo/1.selection-short.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std ;
+
+void Sc(int a[], int n)
+{
+    int i, j, indexMin, temp;
+
+    for(i = 0 ; i < n - 1 ; i++)
+    {
+        indexMin = i;
+        for(j = 0 ; j < n ; j++)
+        {
+
+        }
+    }
+}
