b253779337738d5fb19a46f71426eedec8351254
sajjad-njr <sajjadrahman1020@gmail.com>
2020-12-01 23:14:57 +0600

    string insert function

---
+++ udemy-with-Abdul-Bari/123.2-string-append.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string str = "sanda";
+
+
+    //inset use for data add in fix element in an index
+    str.insert(3,"ji");
+
+    cout << str << endl;
+
+    return 0;
+}
+
