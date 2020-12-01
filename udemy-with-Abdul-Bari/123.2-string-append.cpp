6c4442d2876a53a1caf757dd94cb8adf274849b0
sajjad-njr <sajjadrahman1020@gmail.com>
2020-12-01 23:18:46 +0600

    string apppend

--- udemy-with-Abdul-Bari/123.2-string-append.cpp
+++ udemy-with-Abdul-Bari/123.2-string-append.cpp
@@ -4,12 +4,15 @@ int main()
 {
     string str = "sanda";
 
-
+    cout << str.capacity() << endl;
     //inset use for data add in fix element in an index
     str.insert(3,"ji");
-
     cout << str << endl;
 
+    // how change capacity
+    string str1 = "sandajiiiiiiiiiiiida";
+
+    cout << str1.capacity() << endl;
     return 0;
 }
 
