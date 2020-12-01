23a0e89c47b10637f92f44d56c96fd261d650c8f
sajjad-njr <sajjadrahman1020@gmail.com>
2020-12-02 00:10:02 +0600

    turbo short

--- OJ/code chef/8.TSORT.cpp
+++ OJ/code chef/8.TSORT.cpp
@@ -1,1 +1,22 @@
+#include<bits/stdc++.h>
+using  namespace std ;
+int main()
+{
+    int n , i , j , imin;
+    cin >> n ;
+    int ar[n];
 
+    for(i = 0 ; i< n ; i++)
+    {
+        cin >> ar[i];
+    }
+
+    for(i = 0 ; i < n ; i++)
+    {
+        imin = i ;
+        if(ar[i] < imin)
+            imin = i ;
+    }
+        cout <<  imin  << endl ;
+    return 0;
+}
