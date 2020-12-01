a2519f6ffcfe9852d72ed55a38e9c3f70032741b
sajjad-njr <sajjadrahman1020@gmail.com>
2020-12-01 23:11:44 +0600

    string apppend

--- udemy-with-Abdul-Bari/123.1-string-insert.cpp
+++ udemy-with-Abdul-Bari/123.1-string-insert.cpp
@@ -4,6 +4,9 @@ int main()
 {
     string str = "hello world";
 
+   // sting e kuno element add korar ketre use kora oy
+    str.append("jino ta voro tena tena hoye gele amader vro ");
+
     cout << str << endl;
 
     return 0;
