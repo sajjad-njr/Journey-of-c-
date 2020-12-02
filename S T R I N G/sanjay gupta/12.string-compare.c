ecf8b555d8d19a195c6162673f83660d4d2a0267
sajjad-njr <sajjadrahman1020@gmail.com>
2020-12-02 19:42:27 +0600

    string compare

---
+++ S T R I N G/sanjay gupta/12.string-compare.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+{
+    char a[20] , b[20];
+    int i , f = 0 ;
+
+    gets(a);
+    gets(b);
+
+    for(i = 0 ; i a[i] != '\0' || b[i] = '\0' ; i++)
+    {
+        if(a[i] != b[i])
+            f =  1;
+            break;
+
+    }
+
+}
