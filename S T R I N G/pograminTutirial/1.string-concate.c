32c49b09eabfb2e447def156e322e9b1cd4f77e3
sajjad-njr <sajjadrahman1020@gmail.com>
2020-12-02 22:50:42 +0600

    string concate

--- S T R I N G/pograminTutirial/1.string-concate.c
+++ S T R I N G/pograminTutirial/1.string-concate.c
@@ -3,13 +3,29 @@
 int main()
 {
     char a[100],b[100],c[200];
-    int i = 0 , j = 0 ;
+    int i = 0, j = 0 ;
 
     printf("\n\tEnter string 1 : ");
     gets(a);
     printf("\n\tEnter string 1 : ");
     gets(b);
 
+    while(a[i] != '\0')
+    {
+        c[j] = a[i];
+        j++;
+        i++;
+    }
+    i = 0;
+    while(b[i] != '\0')
+    {
+        c[j] = b[i];
+        j++;
+        i++;
+    }
+    c[j] = '\0';
 
+    printf("\n\n\t String Concatenate : ");
+     puts(c);
     return 0;
 }
