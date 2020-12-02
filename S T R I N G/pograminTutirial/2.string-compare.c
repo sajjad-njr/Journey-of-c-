803f6347aa660b834cc08dbcdc3ab52e6f3b2744
sajjad-njr <sajjadrahman1020@gmail.com>
2020-12-02 23:15:57 +0600

    string comapare

--- S T R I N G/pograminTutirial/2.string-compare.c
+++ S T R I N G/pograminTutirial/2.string-compare.c
@@ -1,7 +1,14 @@
 #include<stdio.h>
+#include<string.h>
+
 int main()
 {
     char a[100],b[100];
     int i = 0 , len1 = 0 , len2=0 , same = 0;
+
+    printf("\nstring 1 : ");
+    gets(a);
+    printf("\nstring 2 : ");
+    gets(b);
 
 }
