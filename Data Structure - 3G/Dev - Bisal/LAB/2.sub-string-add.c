d4a3c9774c21f07528877dde55e1ba34608b75a4
sajjad-njr <sajjadrahman1020@gmail.com>
2020-12-03 02:55:27 +0600

    sub string add

--- Data Structure - 3G/Dev - Bisal/LAB/2.sub-string-add.c
+++ Data Structure - 3G/Dev - Bisal/LAB/2.sub-string-add.c
@@ -1,18 +1,44 @@
 #include<stdio.h>
-
+#include<string.h>
 int main()
 {
     char a[100],b[100],c[100];
 
     int i = 0, j = 0, position = 0, l = 0 ;
-    int x1, x2,x3;
+    int x , x1, x2,x3;
 
     puts("string - 1");
     gets(a);
     puts("string - 2");
     gets(b);
 
+    puts("where you want to add enter position");
+    scanf("%d",&position);
 
+    j = strlen(a);
+    x2 = strlen(b);
 
+    while( i <= j)
+    {
+        c[i] = a[i];
+        i++;
+    }
+    x1 = x2 + j;
+    x3 = position + x2 ;
+
+    for(i = position ; i < x1 ; i++)
+    {
+        x = c[i];
+        if(l < x2 )
+        {
+            a[i] = b[i];
+            l = l + 1;
+        }
+        a[x3] = x ;
+        x3 = x3 + 1 ;
+    }
+
+    puts(a);
+    return 0;
 
 }
