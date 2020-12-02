8268cb04278628ef2bcb557ad6b2e35bd4752443
sajjad-njr <sajjadrahman1020@gmail.com>
2020-12-02 23:46:11 +0600

    string reverse

--- S T R I N G/pograminTutirial/3.string-reverse.c
+++ S T R I N G/pograminTutirial/3.string-reverse.c
@@ -3,9 +3,17 @@
 int main()
 {
     char a[10],b[10];
-    int i , j ;
+    int i = 0, j = 0 ;
 
     gets(a);
 
     for(i = 6 ; a[i] !='\0' ; i--)
+    {
+        b[j] = a[i];
+    }
+    b[j] = '\0';
+
+    puts(b);
+
+    return 0;
 }
