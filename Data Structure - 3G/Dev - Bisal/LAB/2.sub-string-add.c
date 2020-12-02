8ced6525dd887186ad63d8ca108b2f1bc162c66a
sajjad-njr <sajjadrahman1020@gmail.com>
2020-12-03 02:30:59 +0600

    sub string add

--- Data Structure - 3G/Dev - Bisal/LAB/2.sub-string-add.c
+++ Data Structure - 3G/Dev - Bisal/LAB/2.sub-string-add.c
@@ -1,1 +1,18 @@
+#include<stdio.h>
 
+int main()
+{
+    char a[100],b[100],c[100];
+
+    int i = 0, j = 0, position = 0, l = 0 ;
+    int x1, x2,x3;
+
+    puts("string - 1");
+    gets(a);
+    puts("string - 2");
+    gets(b);
+
+
+
+
+}
