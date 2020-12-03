2eb630580ea3d59c62c13675d0e9091f62681587
sajjad-njr <sajjadrahman1020@gmail.com>
2020-12-03 22:29:26 +0600

    array traversing

--- Data Structure - 3G/Dev - Bisal/Array/1.array-Treversing.c
+++ Data Structure - 3G/Dev - Bisal/Array/1.array-Treversing.c
@@ -2,8 +2,20 @@
 
 int main()
 {
-    int a[50],size;
-    pritnf("\enter the array size : ");
+    int a[50],size,i;
+    pritnf("\nenter the array size : ");
     scanf("%d",&size);
 
+    pritnf("\nEnter array element : \n ");
+    for(i= 0; i < size ; i++)
+    {
+        scanf("%d",&a[i]);
+    }
+
+    for(i= 0; i < size ; i++)
+    {
+        scanf("%d",&a[i]);
+    }
+
+    return 0;
 }
