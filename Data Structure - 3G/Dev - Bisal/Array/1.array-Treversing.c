cfc956e088a68e5fe491aec3a28195108c35aaee
sajjad-njr <sajjadrahman1020@gmail.com>
2020-12-03 22:32:33 +0600

    array traversing

--- Data Structure - 3G/Dev - Bisal/Array/1.array-Treversing.c
+++ Data Structure - 3G/Dev - Bisal/Array/1.array-Treversing.c
@@ -1,12 +1,14 @@
+//this is array traversing
+
 #include<stdio.h>
 
 int main()
 {
     int a[50],size,i;
-    pritnf("\nenter the array size : ");
+    printf("\nenter the array size : ");
     scanf("%d",&size);
 
-    pritnf("\nEnter array element : \n ");
+    printf("\nEnter array element : \n ");
     for(i= 0; i < size ; i++)
     {
         scanf("%d",&a[i]);
@@ -14,7 +16,7 @@ int main()
 
     for(i= 0; i < size ; i++)
     {
-        scanf("%d",&a[i]);
+         printf(" %d ",a[i]);
     }
 
     return 0;
