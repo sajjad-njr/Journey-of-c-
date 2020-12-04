f20c17e8bc1b38312b82d432a88afc6de59df80d
sajjad-njr <sajjadrahman1020@gmail.com>
2020-12-04 16:33:57 +0600

    sub string position found

--- Data Structure - 3G/Dev - Bisal/S T R I N G/3.sub-string-position-found.c
+++ Data Structure - 3G/Dev - Bisal/S T R I N G/3.sub-string-position-found.c
@@ -3,7 +3,7 @@
 int main()
 {
     char a[20],b[20];
-    int i = 0 , j = 0 ;
+    int i = 0, j = 0 ;
 
     gets(a);
     gets(b);
@@ -13,11 +13,12 @@ int main()
         if(a[i] == b[0])
         {
             j = 1 ;
-            while(b[j] != '\0' && a[j+i] != '\0' || a[j+1])
+            while(b[j] != '\0' && a[j+i] != '\0' || b[j]==a[j+1])
             {
                 j++;
             }
             if(b[j] == '\0')
+
                 printf("\n\tsub string found at %d position ",i+1);
         }
         i++;
