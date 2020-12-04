9f9ef364b8981c6e584f052b43f6e29d1ae4db95
sajjad-njr <sajjadrahman1020@gmail.com>
2020-12-04 16:07:02 +0600

    string concate

--- Data Structure - 3G/Dev - Bisal/S T R I N G/0.string-concate.c
+++ Data Structure - 3G/Dev - Bisal/S T R I N G/0.string-concate.c
@@ -3,6 +3,23 @@
 int main()
 {
     char a[20], b[20];
-    int a,i,j;
+    int a1 = 0,i = 0 ,j = 0;
+
+    gets(a);
+    gets(b);
+
+    for(i = 0 ; a[i] != '\0' ; i++)
+    {
+        a1++;
+    }
+
+    for(i = a1 , j ; b[j] != '\0' ; i++, j++)    {
+         a[i] = b[j];
+    }
+    a[i] = '\0';
+
+
+    printf("%s",a);
+    return 0;
 
 }
