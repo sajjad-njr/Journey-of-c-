dd65165f4c8b03cd5644b6a0e7964dd0fbfa7168
sajjad-njr <sajjadrahman1020@gmail.com>
2020-12-04 15:18:02 +0600

    sub string insert

--- Data Structure - 3G/Dev - Bisal/S T R I N G/1.insert-sub-string.c
+++ Data Structure - 3G/Dev - Bisal/S T R I N G/1.insert-sub-string.c
@@ -1,8 +1,11 @@
 #include<stdio.h>
+#include<string.h>
+
 int main()
 {
     char a[100],b[100],c[100];
     int i = 0 , j = 0 , pos , l ;
+    int x , x1,x2,x3;
 
     puts("enter main string ");
     gets(a);
@@ -11,6 +14,16 @@ int main()
 
      puts("enter main string ");
      scanf("%d",&pos);
+
+     j = strlen(a);
+     x2 = strlen(b);
+
+    while(i <= j)
+    {
+        c[i] = a[i];
+        i++;
+    }
+     puts(c);
 
     return 0;
 }
