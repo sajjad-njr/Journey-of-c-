84c5e95a108de5d6a3d8af2b2a8ca5205ecb85c0
sajjad-njr <sajjadrahman1020@gmail.com>
2020-12-04 23:45:40 +0600

    pattern matching

--- Data Structure - 3G/Dev - Bisal/S T R I N G/6.pattern-match.c
+++ Data Structure - 3G/Dev - Bisal/S T R I N G/6.pattern-match.c
@@ -1,1 +1,34 @@
+#include<stdio.h>
+#include<string.h>
 
+int main()
+{
+    char t[100],p[100];
+    int k=0, max, r, s,l, cnt, loc;
+
+    gets(t);
+    gets(p);
+
+    r = strlen(p);
+    s = strlen(t);
+    max = s - r + 1 ;
+
+    while(k <= max)
+    {
+        for(l = 0 ; l < r ; l++)
+        {
+            if(p[l] != t[k+l-1])
+            {
+                k++;
+                break;
+            }
+        }
+        if(l==r)
+            printf("\n\tPattern is found at pos = %d\n",k);
+        break;
+    }
+    if(l != r)
+       printf("\n\tPattern is not found ");
+
+    return 0;
+}
