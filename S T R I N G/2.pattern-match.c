2a6c95522004e17a3a165a6e6ea27fa43511616b
sajjad-njr <sajjadrahman1020@gmail.com>
2020-12-05 20:06:51 +0600

    sub sting match or pattern match

---
+++ S T R I N G/2.pattern-match.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+    int K=1 ,Max,S,R,L;
+
+    char T[100],P[100];
+
+    gets(T);
+    gets(P);
+
+    S=strlen(T);
+    R=strlen(P);
+
+    Max=S-R+1;
+
+    while(K<=Max)
+    {
+        for(L=0; L<R; L++)
+        {
+            if(P[L]!=T[K+L-1])
+            {
+                K++;
+                break;
+            }
+        }
+        if(L==R)
+        {
+            printf("Pattern is found at position: %d\n",K);
+            break;
+        }
+    }
+    if(L!=R)
+        printf("Pattern is not found\n");
+
+    return 0;
+}
