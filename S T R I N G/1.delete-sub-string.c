b4dbfd09be71f05e48275379fc9c9337b4fb3c9d
sajjad-njr <sajjadrahman1020@gmail.com>
2020-12-05 19:39:10 +0600

    sub sting remove from string

---
+++ S T R I N G/1.delete-sub-string.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+    int i = 0 , j = 0 , k = 0 , n = 0;
+    int flag = 0 ;
+
+    char str[100], word[100],newStr[100];
+
+    printf("\nEnter a string : ");
+    scanf("%s",&str);
+}
