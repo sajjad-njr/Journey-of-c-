9d43221411b9ed0502d59950b29b8283774df994
sajjad-njr <sajjadrahman1020@gmail.com>
2020-12-05 20:49:54 +0600

    linear search

--- Data Structure - 3G/Dev - Bisal/self try/05-12-2020/1.leaner-Array.c
+++ Data Structure - 3G/Dev - Bisal/self try/05-12-2020/1.leaner-Array.c
@@ -2,6 +2,19 @@
 int main()
 
 {
-    int a[10],i,j;
+    int a[10],i,j,data;
+    int n ;
+
+    printf("\n\tEnter size of array : ");
+    scanf("%d",&n);
+
+    for(i = 0 ; i < n ; i++)
+    {
+        scanf("%d",&a[i]);
+    }
+
+    printf("\n\tEnter searching data: ");
+    scanf("%d",&data);
+
 
 }
