f32652ce6bc223a22052762eed3dd747d18e1931
sajjad-njr <sajjadrahman1020@gmail.com>
2020-12-06 20:44:41 +0600

    sub string delete

---
+++ Data Structure - 3G/Dev - Bisal/self try/05-12-2020/2.sub-string.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+    int i = 0 , j = 0 , k = 0 , n = 0;
+    int flag = 0 ;
+    char str[100],newl[100],word[100];
+
+    printf("\nEnter a string : ");
+    gets(str);
+
+    return 0;
+}
