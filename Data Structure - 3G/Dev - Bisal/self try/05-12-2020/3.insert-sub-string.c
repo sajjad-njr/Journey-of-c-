6d0fe6a14f400b78aef858fe904580bbbcf6ec7c
sajjad-njr <sajjadrahman1020@gmail.com>
2020-12-06 21:02:31 +0600

    sub string delete

---
+++ Data Structure - 3G/Dev - Bisal/self try/05-12-2020/3.insert-sub-string.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+    char a[55],b[55],c[55];
+    int  i = 0 , j = 0 , k = 0 , l = 0;
+
+    int x,x1,x2,x3;
+    puts("\n\tEnter main string");
+    gets(a);
+    puts("\n\tEnter the word where you want : ");
+    gets(b);
+
+    return 0;
+}
