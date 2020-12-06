581378a2e4b4cfa6c867684ca7684e6644a3f82d
sajjad-njr <sajjadrahman1020@gmail.com>
2020-12-07 03:22:37 +0600

    sub string match

--- Data Structure - 3G/Dev - Bisal/self try/7-12-2020/1.pattern-match.c
+++ Data Structure - 3G/Dev - Bisal/self try/7-12-2020/1.pattern-match.c
@@ -3,11 +3,35 @@
 
 int main()
 {
-    int i = 0 , k , max , l = 0;
+    int i = 0 , k = 0, s , r ,  max , l = 0 ;
     char t[100],p[100];
 
     gets(t);
     gets(p);
+
+    s = strlen(t);
+    r = strlen(p);
+    max = s - r + 1 ;
+
+    while(k <= max)
+    {
+        for(i = 0 ; i < r ; i++)
+        {
+            if(p[i] != t[k+i-1])
+            {
+                k++;
+                break;
+            }
+        }
+        if(i == r)
+        {
+            printf("\n\tpattern is found at position : %d",k);
+             break;
+        }
+
+    }
+    if(i != r)
+        printf("\n\tpattern is not found");
 
     return 0;
 }
