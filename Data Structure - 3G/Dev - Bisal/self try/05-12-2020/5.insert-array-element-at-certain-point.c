929cae20e4ddaf1e7d1939af73e329fedbc70471
sajjad-njr <sajjadrahman1020@gmail.com>
2020-12-06 22:34:45 +0600

    linear array search

--- Data Structure - 3G/Dev - Bisal/self try/05-12-2020/5.insert-array-element-at-certain-point.c
+++ Data Structure - 3G/Dev - Bisal/self try/05-12-2020/5.insert-array-element-at-certain-point.c
@@ -2,19 +2,32 @@
 int main()
 {
     int a[10];
-    int i = 0 ,size;
+    int i = 0,size,data,count = 0;
 
     scanf("%d",&size);
     printf("\n\tEnter element of array : ");
 
-    for(;i < size;i++)
+    for(; i < size; i++)
     {
         scanf("%d",&a[i]);
     }
-    printf("\n\tafter insert : ");
-    for(i = 0 ;i < size;i++)
+
+    printf("\n\tinter the searching data  : ");
+    scanf("%d",&data);
+
+    for(i = 0 ; i < size; i++)
     {
-         printf(" %d ",a[i]);
+        if(a[i] == data)
+        {
+            count = 1;
+            break;
+        }
+
+
     }
+    if(count == 0)
+        printf("\n\twrong data ");
+    else
+        printf("\n\t data found at position %d",i+1);
     return 0;
 }
