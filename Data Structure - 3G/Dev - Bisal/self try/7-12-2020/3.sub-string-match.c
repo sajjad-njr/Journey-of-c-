4f7696157921a125560aa87d667672dd8ba75427
sajjad-njr <sajjadrahman1020@gmail.com>
2020-12-07 22:20:25 +0600

    sub string match

--- Data Structure - 3G/Dev - Bisal/self try/7-12-2020/3.sub-string-match.c
+++ Data Structure - 3G/Dev - Bisal/self try/7-12-2020/3.sub-string-match.c
@@ -4,8 +4,15 @@
 int main()
 {
     char str1[100],str2[100];
-    int i = 0 , j = 0 , k = 0 ;
+    int i = 0 , j = 0 , k = 0 ,s,r,max = 0;
 
     gets(str1);
     gets(str2);
+
+    r = strlen(str1);
+    s = strlen(str2);
+
+    max = r - s + 1;
+
+    return 0;
 }
