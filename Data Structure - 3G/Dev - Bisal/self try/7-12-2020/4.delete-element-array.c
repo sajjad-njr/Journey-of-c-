b51eab4d6ccf24e49c0a2f8bfee102e174a32b37
sajjad-njr <sajjadrahman1020@gmail.com>
2020-12-07 23:08:49 +0600

    delete array

--- Data Structure - 3G/Dev - Bisal/self try/7-12-2020/4.delete-element-array.c
+++ Data Structure - 3G/Dev - Bisal/self try/7-12-2020/4.delete-element-array.c
@@ -1,10 +1,21 @@
 #include<stdio.h>
 int main()
 {
-    int pos,i=0;
+    int n ,pos = 0,i=0;
+    n = 10;
     int a[10] = {34,56,76,56,34,21,53,75,84,50};
 
-    for(i = 0 ; i < 10 ; i++)
+    printf("\n\tEnter position to delete : ");
+    scanf("%d",&pos);
+
+    pos = pos - 1;
+
+    for(i = pos ; i < n - 1; i++)
+    {
+        a[i] = a[i+1];
+    }
+    n = n -1 ;
+    for(i = 0 ; i < n; i++)
     {
         printf(" %d ",a[i]);
     }
