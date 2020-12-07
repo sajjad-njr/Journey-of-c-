e04855342c05eeb265b16d5e8a2dffafcaf0c13e
sajjad-njr <sajjadrahman1020@gmail.com>
2020-12-07 22:32:27 +0600

    sub string match

--- Data Structure - 3G/Dev - Bisal/self try/7-12-2020/3.sub-string-match.c
+++ Data Structure - 3G/Dev - Bisal/self try/7-12-2020/3.sub-string-match.c
@@ -4,7 +4,7 @@
 int main()
 {
     char str1[100],str2[100];
-    int i = 0 , j = 0 , k = 0 ,s,r,max = 0;
+    int i = 0, j = 0, k = 0,s,r,max = 0;
 
     gets(str1);
     gets(str2);
@@ -13,6 +13,25 @@ int main()
     s = strlen(str2);
 
     max = r - s + 1;
+
+    while(k <= max)
+    {
+        for(i = 0 ; i < s ; i++)
+        {
+            if(str2[i] != str1[k  + i - 1])
+            {
+                k++;
+                break;
+            }
+        }
+        if(i == s)
+        {
+            printf("\n\tsub string match at position : %d",k);
+            break;
+        }
+    }
+    if(i != s)
+        printf("\n\tsub string not match  ");
 
     return 0;
 }
