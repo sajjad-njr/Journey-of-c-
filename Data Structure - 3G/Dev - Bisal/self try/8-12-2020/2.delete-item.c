8888615cc3d2efd083cd2aab364f9e56e28add04
sajjad-njr <sajjadrahman1020@gmail.com>
2020-12-08 22:35:31 +0600

    delete element

--- Data Structure - 3G/Dev - Bisal/self try/8-12-2020/2.delete-item.c
+++ Data Structure - 3G/Dev - Bisal/self try/8-12-2020/2.delete-item.c
@@ -1,2 +1,21 @@
 #include<stdio.h>
+
 int main()
+{
+    int a[10];
+    int i = 0,size,pos = 0,element = 0;
+
+    printf("\n\tEnter size of array : ");
+    scanf("%d",&size);
+
+    printf("\n\tEnter element of array : ");
+
+    for(i = 0; i < size; i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    printf("\n\tEnter array  insert position : ");
+    scanf("%d",&pos);
+    printf("\n\tEnter array  insert  element : ");
+    scanf("%d",&data);
+}
