e160ee6cd63dcbba894741d53720e5883051835b
sajjad-njr <sajjadrahman1020@gmail.com>
2020-12-08 20:58:08 +0600

    insert element

---
+++ Data Structure - 3G/Dev - Bisal/self try/8-12-2020/1.2-insert-item.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+    int a[1000];
+    int i = 0 , pos = 0 ,n;
+
+    printf("\n\tEnter array size : ")
+    scanf("%d",&n);
+
+    for(i = 0 ; i < n ; i++)
+    {
+        scanf("%d",&n);
+    }
+
+}
