e9aced501a4909e785fb7d651fd56467d50803e6
sajjad-njr <sajjadrahman1020@gmail.com>
2020-12-09 20:46:17 +0600

    sub string delete

--- Data Structure - 3G/Dev - Bisal/self try/09-12-2020/2.insert-sub-string.c
+++ Data Structure - 3G/Dev - Bisal/self try/09-12-2020/2.insert-sub-string.c
@@ -3,5 +3,14 @@
 
 int main()
 {
+    char a[100],b[100],c[100];
+    int i = 0, j = 0 , k = 0 , l =0 ;
+    int x,x1,x2,x3;
+
+    puts("enter a main string");
+    gets(a);
+
+    puts("enter sub string");
+    gets(b);
     return 0;
 }
