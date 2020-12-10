482644ce7bf231d2cf447d4590fdb401825b5819
sajjad-njr <sajjadrahman1020@gmail.com>
2020-12-10 23:09:22 +0600

    delete element of array

--- Data Structure - 3G/Dev - Bisal/self try/09-12-2020/5.delete-element-of-array.c
+++ Data Structure - 3G/Dev - Bisal/self try/09-12-2020/5.delete-element-of-array.c
@@ -2,7 +2,7 @@
 int main()
 {
     int a[5] = {45,67,87,98,54};
-    int n = 5;
+    int n = 5,i;
     printf("\n\tbefore delete array : \n");
 
     for(i = 0 ; i < n ; i++)
@@ -14,14 +14,14 @@ int main()
     printf("\n\tEnter position where you want to delete : ");
     scanf("%d",&pos);
 
-    printf("\n\tbefore delete array : \n");
+    //printf("\n\tbefore delete array : \n");
 
-    for(i = pos ; i < 5 ; i++)
+    for(i = pos - 1 ; i < 5 ; i++)
     {
-        a[i+1] = a[i];
+        a[i] = a[i + 1];
     }
-    n = n - 1 ;
-    for(i = 0 ; i < n ; i++)
+    n = 4 ;
+    for(i = 0 ; i < n  ; i++)
     {
         printf("  %d  ",a[i]);
     }
