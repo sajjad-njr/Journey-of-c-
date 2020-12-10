4e8fa15630ab2626fb574df75c0c308b57e941cd
sajjad-njr <sajjadrahman1020@gmail.com>
2020-12-10 22:53:12 +0600

    traverse array element

---
+++ Data Structure - 3G/Dev - Bisal/self try/09-12-2020/4.traverse-element.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main()
+{
+    int n = 10 ,a[10] = {45,56,7,78,9,87,54,32,29,987};
+    int i , j,pos,element ;
+
+    for(i = 0 ; i <n ; i++)
+    {
+        printf(" %d ",a[i]);
+    }
+
