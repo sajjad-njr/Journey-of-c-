9c15e0a6cb392351f820e8cfc60fe521da558006
sajjad-njr <sajjadrahman1020@gmail.com>
2020-12-10 22:44:52 +0600

    insert element

--- Data Structure - 3G/Dev - Bisal/self try/09-12-2020/3.insert-element.c
+++ Data Structure - 3G/Dev - Bisal/self try/09-12-2020/3.insert-element.c
@@ -3,13 +3,15 @@
 int main()
 {
     int a[10] = {45,56,7,78,9,87,54,32,29,987};
-    int i , j ;
+    int i , j,pos,element ;
 
     for(i = 0 ; i < 10 ; i++)
     {
         printf(" %d ",a[i]);
     }
 
+    printf("\nenter an element ")
+    scanf("%d",&element);
 
 
 
