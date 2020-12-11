5ca0e1cbd801a2a1e45babc690bfe64510d3e729
sajjad-njr <sajjadrahman1020@gmail.com>
2020-12-11 17:51:05 +0600

    2.1 algo find max value

--- Data Structure - 3G/Dev - Bisal/self try/10-12-2020/2.1-algorithm.c
+++ Data Structure - 3G/Dev - Bisal/self try/10-12-2020/2.1-algorithm.c
@@ -1,1 +1,21 @@
+//find maximum value
+#include<stdio.h>
 
+int main()
+{
+    int a[5] = {67,89,99,554,32};
+    int i , loc = 0 , max = a[0];
+
+    for(i = 0 ; i < 5 ; i++)
+    {
+        if(a[i] > max)
+        {
+             max = a[i];
+             loc = i;
+        }
+    }
+
+    printf("\n\tThe maximum value is : %d and find position %d",max,loc+1);
+
+    return 0;
+}
