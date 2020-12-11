d3a2b87b451193a3465edb817abc90cfb772a778
sajjad-njr <sajjadrahman1020@gmail.com>
2020-12-11 18:26:00 +0600

    linear search

---
+++ Data Structure - 3G/Dev - Bisal/self try/10-12-2020/2.4-linear-search.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+int main()
+{
+    int data[100];
+    int i,k, item, loc = 0;
+
+    printf("\n\tEnter the size of a array : ");
+    scanf("%d",&k);
+    printf("\n\tEnter the elemrnt of a array : ");
+
+    for(i = 0 ; i < k ; i++ )
+    {
+        scanf("%d",&data[i]);
+
+    }
+
+    printf("\n\tEnter the search item : ");
+    scanf("%d",&item);
+
+    for(i = 0 ; i < k ; i++ )
+    {
+        if(data[i] == item)
+        {
+            loc =  i ;
+            break;
+        }
+
+    }
+
+    if(loc = 0)
+        printf("\n\t:Item is not found ");
+    else
+        printf("\n\t %d is the location of item = %d",loc+1,item);
+    return 0;
+}
