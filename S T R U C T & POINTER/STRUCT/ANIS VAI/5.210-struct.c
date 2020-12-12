e5cf5b5dbb6eae4b363c050d22f260634e7cd0f1
sajjad-njr <sajjadrahman1020@gmail.com>
2020-12-12 15:32:11 +0600

    struct initial declare

---
+++ S T R U C T & POINTER/STRUCT/ANIS VAI/5.210-struct.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+struct wassi
+{
+    int age ;
+    float salary;
+};
+
+
+
+int main()
+{
+    struct wassi f;
+
+    f.age = 19;
+    f.salary = 4567.76;
+
+    printf("preson age = %d and salary = %.2f",f.age,f.salary);
+
+    return 0;
+}
