a4f4fe3de32adcb946c11a61a860bfa60ca86b38
sajjad-njr <sajjadrahman1020@gmail.com>
2020-12-12 16:23:50 +0600

    struct with array

--- S T R U C T & POINTER/STRUCT/ANIS VAI/5.214-struct-array.c
+++ S T R U C T & POINTER/STRUCT/ANIS VAI/5.214-struct-array.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 
-struct information()
+struct information
 {
-    int age ;
+    int age;
     float salary;
 };
 
@@ -14,6 +14,20 @@ int main()
 
     for(i = 0 ; i < 4 ; i++)
     {
-        printf("\n\t")
+        printf("\n\tEnter the person %d INFORMATION \n",i+1);
+        printf("\n\tEnter person age = ");
+        scanf("%d",&person[i].age);
+        printf("\n\tEnter person  salary = ");
+        scanf("%d",&person[i].salary);
     }
+
+    for(i = 0 ; i < 4 ; i++)
+    {
+        printf("\n\tThe person %d INFORMATION \n",i+1);
+        printf("\n\tPerson age = %d ",person[i].age);
+
+        printf("\n\tPerson  salary = %.2f",person[i].salary);
+       }
+
+    return 0;
 }
