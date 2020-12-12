8685f5635eacd239fe8bca2378db6014fddc4f74
sajjad-njr <sajjadrahman1020@gmail.com>
2020-12-12 16:17:43 +0600

    struct with array

---
+++ S T R U C T & POINTER/STRUCT/ANIS VAI/5.214-struct-array.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+struct information()
+{
+    int age ;
+    float salary;
+};
+
+int main()
+{
+    struct information person[4];
+
+    int i ;
+
+    for(i = 0 ; i < 4 ; i++)
+    {
+        printf("\n\t")
+    }
+}
