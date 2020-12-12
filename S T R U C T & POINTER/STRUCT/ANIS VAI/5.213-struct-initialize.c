7eba53c2b69e5187b48b85b2ddb69f91c44a4932
sajjad-njr <sajjadrahman1020@gmail.com>
2020-12-12 15:57:43 +0600

    struct initialize

--- S T R U C T & POINTER/STRUCT/ANIS VAI/5.213-struct-initialize.c
+++ S T R U C T & POINTER/STRUCT/ANIS VAI/5.213-struct-initialize.c
@@ -13,5 +13,14 @@ int main()
     printf("preson age = %d and salary = %.2f",person1.age,person1.salary);
     // struct person person2 , person3;
 
+    struct wassi person2,person3;
+    person2.age = 50;
+    person2.salary = 9090.90;
+
+
+    person3 = person2;
+
+    printf("\n\npreson 2  age = %d and salary = %.2f",person2.age,person2.salary);
+    printf("\n\npreson 3  age = %d and salary = %.2f",person3.age,person3.salary);
     return 0;
 }
