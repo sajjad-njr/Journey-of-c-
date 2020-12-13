7ee3d9b64f1ce250ca479a79a9777701563074b4
sajjad-njr <sajjadrahman1020@gmail.com>
2020-12-13 22:33:37 +0600

    entry data

--- Z Self Prectice/exam night/3.insert.c
+++ Z Self Prectice/exam night/3.insert.c
@@ -1,1 +1,35 @@
+#include<stdio.h>
+
+int main()
+{
+    int a[50],size,i,element,pos;
+    printf("\nenter the array size : ");
+    scanf("%d",&size);
+
+    printf("\nEnter array element : \n ");
+    for(i= 0; i < size ; i++)
+    {
+        scanf("%d",&a[i]);
+    }
+
+    int item;
+    int pos ;
+
+    scanf("%d",&pos);
+
+    scanf("%d",&item);
+
+    for(i = 5 ; i >= pos - 1 ; i--)
+    {
+        a[i+1]=a[i];
+    }
+    a[pos-1] = item;
+
+    for(i = 0 ; i < 6 ; i++)
+    {
+        printf("%d",a[i]);
+    }
+
+    return 0;
+}
 
