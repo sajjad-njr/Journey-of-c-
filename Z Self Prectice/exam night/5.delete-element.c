754b28e46d56245d9a4a12670122589f58084a0b
sajjad-njr <sajjadrahman1020@gmail.com>
2020-12-14 23:08:10 +0600

    delete element array data

--- Z Self Prectice/exam night/5.delete-element.c
+++ Z Self Prectice/exam night/5.delete-element.c
@@ -1,7 +1,18 @@
 #include<stdio.h>
 int main()
 {
-    int a[50], i , size;
+    int a[50], i , size, pos ;
     scanf("%d",&size);
-    for()
+    for(i = 0 ; i < size ; i++)
+    {
+        scanf("%d",&a[i]);
+    }
+
+    scanf("%d",&size);
+    for(i = 0 ; i < size ; i++)
+    {
+        scanf("%d",&a[i]);
+    }
+
+    return 0;
 }
