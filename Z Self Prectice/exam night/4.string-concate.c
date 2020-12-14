b6284beff0d0f6b31a22b0181b7b5ec5343b1c34
sajjad-njr <sajjadrahman1020@gmail.com>
2020-12-14 22:54:46 +0600

    string concate

--- Z Self Prectice/exam night/4.string-concate.c
+++ Z Self Prectice/exam night/4.string-concate.c
@@ -3,10 +3,16 @@
 int main()
 {
     char str1[100],str2[100];
-    int i , j ;
+    int i , j , len = 0 ;
     printf("\nEnter two strings = ");
     gets(str1);
     gets(str2);
+
+    for(i = 0 ; str1[i] != '\0' ; i++)
+    {
+        len = len + 1;
+
+    }
 
     return 0;
 }
