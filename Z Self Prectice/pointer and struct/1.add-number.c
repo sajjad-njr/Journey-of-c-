622efa60c4007275a2f48fb51de6f31418393995
sajjad-njr <sajjadrahman1020@gmail.com>
2020-12-15 21:43:24 +0600

    add two number

--- Z Self Prectice/pointer and struct/1.add-number.c
+++ Z Self Prectice/pointer and struct/1.add-number.c
@@ -1,8 +1,20 @@
 #include<stdio.h>
 
-int (*ptr) add(int c, int b)
+int *add (int c, int b)
 {
-    int a = c + b ;
+    int a;
+    a = c + b ;
     return a ;
 }
 
+int main()
+{
+    int  a = 5 , b = 7 , f;
+
+    int *c;
+    c = &add ;
+
+    f = c(5,7);
+
+    printf("%d",c);
+}
