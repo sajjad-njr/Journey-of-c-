572d260545f42ab9cf0acd15fbf8f9645b6fb84b
sajjad-njr <sajjadrahman1020@gmail.com>
2020-12-16 21:17:14 +0600

    atm

--- OJ/code chef/9.ATM.cpp
+++ OJ/code chef/9.ATM.cpp
@@ -11,15 +11,16 @@ int main()
 
     if(x>y)
         cout << fixed << setprecision(2) << y <<endl;
+
     else
     {
-        if(x%5==0)
+        if(x%5==0 && x < y + .50)
         {
             c = y - x - 0.50;
             cout << fixed << setprecision(2) <<  c <<endl;
         }
         else
-             cout << fixed << setprecision(2) <<  y <<endl;
+            cout << fixed << setprecision(2) <<  y <<endl;
     }
     return 0;
 }
