99ac056ec9ed49caa44a5d41489c718a2ba8ef30
sajjad-njr <sajjadrahman1020@gmail.com>
2020-12-17 21:35:04 +0600

    string append

--- udemy-with-Abdul-Bari/123.1-string-append.cpp
+++ udemy-with-Abdul-Bari/123.1-string-append.cpp
@@ -4,7 +4,12 @@ using namespace std ;
 int main()
 {
     string str = " a country";
+
     str.append(" Bangladesh ");
+
+    cout << str << endl;
+
+    str.append("  independent country  ");
 
     cout << str << endl;
 
