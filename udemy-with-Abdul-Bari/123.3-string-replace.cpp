9c695876affdcee690f9eb54179778346f02b23c
sajjad-njr <sajjadrahman1020@gmail.com>
2020-12-17 21:45:44 +0600

    string replace

--- udemy-with-Abdul-Bari/123.3-string-replace.cpp
+++ udemy-with-Abdul-Bari/123.3-string-replace.cpp
@@ -6,13 +6,12 @@ int main()
     string str = "ProgramingHero";
 
 
-    /*str.replace(3,4," Mr Sajjad ");
+    str.replace(3,4," Mr Sajjad ");
 
     cout << str ;
-*/
-    str.replace(3," Mr Sajjad ");
 
-    cout << str ;
+    //str.replace(3," Mr Sajjad ");  // error show that message
+
 
     return 0 ;
 }
