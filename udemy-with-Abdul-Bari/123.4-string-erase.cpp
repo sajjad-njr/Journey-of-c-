afb230b7928d47f540d889e88b6829b017b8191f
sajjad-njr <sajjadrahman1020@gmail.com>
2020-12-17 21:47:43 +0600

    string erase

---
+++ udemy-with-Abdul-Bari/123.4-string-erase.cpp
@@ -0,0 +1,15 @@
+// erase means CLEAR()
+
+#include<bits/stdc++.h>
+using namespace std ;
+
+int main()
+{
+    string str = "ProgramingHero";
+
+    str.erase();
+
+    cout << str ;
+
+    return 0 ;
+}
