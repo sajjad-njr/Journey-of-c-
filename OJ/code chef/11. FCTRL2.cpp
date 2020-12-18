703469c3af6d87f677973364161c753db289f3b2
sajjad-njr <sajjadrahman1020@gmail.com>
2020-12-19 00:26:20 +0600

    FCTRL2

--- OJ/code chef/11. FCTRL2.cpp
+++ OJ/code chef/11. FCTRL2.cpp
@@ -1,1 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std ;
 
+int main()
+{
+    int t, a ;
+    cin >> t;
+
+    while(t--)
+    {
+        int m = 1, fact = 1 ;
+        cin >> a;
+        for(int i =  1 ; i < a ; i++)
+        {
+            m = fact *( a - i);
+        }
+
+        cout << m << endl;
+    }
+
+    return 0;
+}
