7e295f8af6e74fb7bbfc7245fc4edd64fbbc0216
sajjad-njr <sajjadrahman1020@gmail.com>
2020-12-18 23:50:37 +0600

    acess and memory location

--- S T R U C T & POINTER/S AN JAY/POINTER/4-acess-and-modify-memory-location.c
+++ S T R U C T & POINTER/S AN JAY/POINTER/4-acess-and-modify-memory-location.c
@@ -8,12 +8,18 @@ int main()
 
     a = a + *p;
     printf("\nThe value of a = %d",a);
-    printf("\nThe value of a = %d",*p);
+    printf("\nThe value of p = %d",*p);
 
     *p = *p + a;
     printf("\n\n\n\n");
     printf("\nThe value of a = %d",a);
-    printf("\nThe value of a = %d",*p);
+    printf("\nThe value of p = %d",*p);
+
+    a =a + 30;
+
+     printf("\n\n\n\n");
+    printf("\nThe value of a = %d",a);
+    printf("\nThe value of p = %d",*p);
 
     return 0;
 }
