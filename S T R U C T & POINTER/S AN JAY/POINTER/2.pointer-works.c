3818d29e1e0c3a5a683e415dcb200e6419c6004b
sajjad-njr <sajjadrahman1020@gmail.com>
2020-12-18 22:20:00 +0600

    pointer initialize

--- S T R U C T & POINTER/S AN JAY/POINTER/2.pointer-works.c
+++ S T R U C T & POINTER/S AN JAY/POINTER/2.pointer-works.c
@@ -3,5 +3,12 @@ int main()
 {
     int a = 9 ;
     int *p;
+    p = &a;
+    printf("\nEnter a value = %d",a);
+     printf("\nEnter a value = %d",*p);
 
+      printf("\nEnter a value = %p",&a);
+       printf("\nEnter a value = %p",p);
+
+       return 0;
 }
