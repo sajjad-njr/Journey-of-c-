1eb295e076cdd81ae0d71c3e54aaa511dfca161a
sajjad-njr <sajjadrahman1020@gmail.com>
2020-12-19 22:01:30 +0600

    add two number by using pointer

--- S T R U C T & POINTER/S AN JAY/POINTER/5.add-two-number.c
+++ S T R U C T & POINTER/S AN JAY/POINTER/5.add-two-number.c
@@ -1,10 +1,15 @@
 #include<stdio.h>
 int main()
 {
-    int a , b , c , *p1,*p2, *p3;
+    int a , b , c , *p1,*p2, p3;
 
     printf("Enter a number = ");
     scanf("%d %d",&a,&b);
 
+    p1 = &a;
+    p2 = &b;
+
+    p3 = *p1 + *p2;
+    printf("\nsum = %d",p3);
     return 0;
 }
