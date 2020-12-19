4a9e3482c0246c659b6c9f346f2626f6ee57816c
sajjad-njr <sajjadrahman1020@gmail.com>
2020-12-19 22:05:27 +0600

    genral factorial

--- S T R U C T & POINTER/S AN JAY/POINTER/6.normal-factorial.c
+++ S T R U C T & POINTER/S AN JAY/POINTER/6.normal-factorial.c
@@ -2,6 +2,16 @@
 
 int main()
 {
-    int n , f = 1;
-    printf("enter a nnumber");
+    int i , n , f = 1;
+    printf("enter a number");
+
+    scanf("%d",&n);
+
+    for(i = 1 ; i <= n ; i++)
+    {
+        f = f * i;
+    }
+
+    printf("%d",f);
+    return 0;
 }
