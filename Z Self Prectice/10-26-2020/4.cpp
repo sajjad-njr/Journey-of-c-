f86d6bddb7c5c535baf478c5eb7f04719b338c51
sajjad-njr <sajjadrahman1020@gmail.com>
2020-12-20 22:25:14 +0600

    random task

--- Z Self Prectice/10-26-2020/4.cpp
+++ Z Self Prectice/10-26-2020/4.cpp
@@ -5,23 +5,30 @@ int main()
     int b;
     //scanf("%d",&b);
 
-   /* while(b--)
-    {
-        scanf("%d",&a);
-        if(a == 42)
-            break;
-        else
+    /* while(b--)
+     {
+         scanf("%d",&a);
+         if(a == 42)
+             break;
+         else
 
-            for(i = 0 ; i!= 4 ; i++)
-            {
-                printf(" %d ",i);
-            }
-        printf("\n");
-    }
-    */
+             for(i = 0 ; i!= 4 ; i++)
+             {
+                 printf(" %d ",i);
+             }
+         printf("\n");
+     }
+     */
     for(int i = 0 ; i < 100 ; i++)
     {
         printf(" %d ",i+1);
+    }
+    printf("\n"); printf("\n");
+
+    for(int i = 0 ; i < 100 ; i++)
+    {
+        if(i%2 == 0)
+            printf(" %d ",i);
     }
     return 0;
 }
