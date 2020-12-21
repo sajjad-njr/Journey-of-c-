4baa3c11d8d2f45f5d05a2fabee82b23181e46d1
sajjad-njr <sajjadrahman1020@gmail.com>
2020-12-21 22:56:41 +0600

    trubo

--- Z Self Prectice/10-26-2020/turbo-shot.c
+++ Z Self Prectice/10-26-2020/turbo-shot.c
@@ -2,13 +2,31 @@
 int main()
 {
     int a;
-    int i ,j ;
+    int i,j,low;
     int b[100];
 
     scanf("%d",&a);
     for(i = 0 ; i < a ; i++)
     {
         scanf("%d",&b[i]);
+
+    }
+
+
+    for(i = 0 ; i < a ; i++)
+    {
+        low = b[i];
+        for(j = 0 ; j < a - 1 ; j++ )
+        {
+            if(low[j] > b[j])
+                low[j] = b[j];
+        }
+
+    }
+
+    for(i = 0 ; i < a ; i++)
+    {
+        printf("\n\t%d",a[i]);
 
     }
     return 0;
