423d510b8a95ffaf1c700e94fcc5b891f730f8b9
sajjad-njr <sajjadrahman1020@gmail.com>
2020-12-23 01:13:26 +0600

    template demo

--- udemy-with-Abdul-Bari/138.function-teampalte.cpp
+++ udemy-with-Abdul-Bari/138.function-teampalte.cpp
@@ -1,14 +1,19 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+// use for any data type
 template<class t>
-t maxim(t a , t b)
+t maxim(t a, t b)
 {
     return a > b ? a : b ;
 }
 
 int main()
 {
+    // Int data type use to TEMPLATE
     cout << maxim(8,9) << endl;
+
+    // float value use to TEMPLATE
+    cout << maxim(88.8f,9.6f) << endl;
     return 0;
 }
