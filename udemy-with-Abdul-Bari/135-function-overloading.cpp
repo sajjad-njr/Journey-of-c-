1869f83e4d43024585b3b6a88933796244d8e259
sajjad-njr <sajjadrahman1020@gmail.com>
2020-12-22 18:57:59 +0600

    function overloading

--- udemy-with-Abdul-Bari/135-function-overloading.cpp
+++ udemy-with-Abdul-Bari/135-function-overloading.cpp
@@ -5,17 +5,25 @@ int sum (int q, int e)
 {
     return q + e;
 }
+
 int sum (int q, int e, int y )
 {
     return q + e + y;
+}
+float  sum (float o, float p)
+{
+    return o + p ;
 }
 
 
 int main()
 {
     int a, b,v,k;
+    float  m, n ;
+
 
     cin >> a >> b >> v;
+    cin >> m >> n ;
 
     k = sum(a, b);
 
@@ -23,6 +31,7 @@ int main()
 
     cout << sum(a, b, v) << endl;
 
+    cout << sum(m,n);
 
     return 0;
 }
