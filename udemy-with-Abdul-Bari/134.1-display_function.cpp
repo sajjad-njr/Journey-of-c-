a81d568839530b42912090b3786e121aab2a2f50
sajjad-njr <sajjadrahman1020@gmail.com>
2020-12-22 18:14:39 +0600

    display demo function

---
+++ udemy-with-Abdul-Bari/134.1-display_function.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using  namespace std;
+
+void display()
+{
+    cout << "Hello Function" << endl;
+}
+void display1()
+{
+     cout << "Ki obstta " << endl;
+}
+
+int main()
+{
+    display();
+
+    display1();
+    return 0;
+}
