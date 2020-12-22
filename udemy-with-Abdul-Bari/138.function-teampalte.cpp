e5b56682fb14e1737d7657962dc87c964114240e
sajjad-njr <sajjadrahman1020@gmail.com>
2020-12-23 01:10:49 +0600

    template demo

---
+++ udemy-with-Abdul-Bari/138.function-teampalte.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+template<class t>
+t maxim(t a , t b)
+{
+    return a > b ? a : b ;
+}
+
+int main()
+{
+    cout << maxim(8,9) << endl;
+    return 0;
+}
