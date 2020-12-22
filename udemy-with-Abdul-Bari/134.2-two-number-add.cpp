6670450c18954e36f72192cd683877bf64aa8527
sajjad-njr <sajjadrahman1020@gmail.com>
2020-12-22 18:20:28 +0600

    add two number using function

--- udemy-with-Abdul-Bari/134.2-two-number-add.cpp
+++ udemy-with-Abdul-Bari/134.2-two-number-add.cpp
@@ -1,9 +1,20 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int add(int a , int b)
+int add(int a, int b)
 {
     int c ;
     c = a + b ;
     return c ;
+}
+
+int main()
+{
+    int d, f, g ;
+    g = d + f;
+
+    cin >> d >> f ;
+    g = add(d,f);
+    cout << g ;
+    return 0;
 }
