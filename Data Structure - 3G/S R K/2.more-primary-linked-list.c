a86504ab4b9d4f2e2fafdee165c2a93a0156b2ba
sajjad-njr <sajjadrahman1020@gmail.com>
2020-12-23 21:23:42 +0600

    2 more about  primary linked list

--- Data Structure - 3G/S R K/2.more-primary-linked-list.c
+++ Data Structure - 3G/S R K/2.more-primary-linked-list.c
@@ -1,8 +1,24 @@
 #include<stdio.h>
-
+#include<stdlib.h>
 typedef struct node
 {
     int data ;
     struct node *next ;
 } Node;
 
+void main()
+{
+    Node *h , *m;
+
+    h = (Node*) malloc(sizeof(Node));
+    m = (Node*) malloc(sizeof(Node));
+
+    h->data = 890;
+    h->next = NULL ;
+
+     m->data = 1890;
+    m->next = NULL ;
+
+    printf("\n\tH node = %d \t Node address = %u",h->data,h->next);
+
+}
