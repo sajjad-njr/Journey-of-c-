be1bd0a9017545ab7672ea9786edfa73309c4339
sajjad-njr <sajjadrahman1020@gmail.com>
2020-12-23 21:08:11 +0600

    primary linked list

--- Data Structure - 3G/S R K/1.primary-node.c
+++ Data Structure - 3G/S R K/1.primary-node.c
@@ -4,3 +4,12 @@ typedef struct node{
     int data ;
     struct node *next;
 }Node;
+
+void main()
+{
+    Node h ;
+    h.data = 89;
+    h.next = NULL;
+
+    printf("\n\tNode value = %d \t Node address = %u",h.data,h.next);
+}
