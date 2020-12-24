39437492f143f74cee6099bd132b1ed96e2959f3
sajjad-njr <sajjadrahman1020@gmail.com>
2020-12-24 22:37:20 +0600

    linked list

--- Z Self Prectice/End of the week/222.linked-list.c
+++ Z Self Prectice/End of the week/222.linked-list.c
@@ -15,4 +15,15 @@ int main()
     h1 = (Node*) malloc (sizeof(Node));
     h2 = (Node*) malloc (sizeof(Node));
     h3 = (Node*) malloc (sizeof(Node));
+
+    h1->data = 1;
+    h1->next = h2;
+
+    h2->data = 100;
+    h2->next = h3;
+
+    h2->data = 19700;
+    h2->next = NULL;
+
+    printf()
 }
