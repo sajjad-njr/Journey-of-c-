669edfb13cb2f18f5bde60d4ed84997590f7e269
sajjad-njr <sajjadrahman1020@gmail.com>
2020-12-24 23:20:06 +0600

    linked list  for 385 pdf

--- Z Self Prectice/End of the week/223.linked-list.c
+++ Z Self Prectice/End of the week/223.linked-list.c
@@ -106,3 +106,134 @@ void Display()
     {
         printf ("%d ", q->info);
         q=q->link;
+    }
+    printf ("\n");
+    getch();
+}/*End of display() */
+//Function to count the number of nodes in the linked list
+void Count()
+{
+    struct node *q=start;
+    int cnt=0;
+    while(q!=NULL)
+    {
+        q=q->link;
+        cnt++;
+    }
+    printf ("Number of elements are %d\n",cnt);
+    getch();
+}/*End of count()*/
+//This function will reverse the linked list
+void Rev()
+{
+    struct node *p1,*p2,*p3;
+    if(start->link==NULL) /*only one element*/
+        return;
+    p1=start;
+    p2=p1->link;
+    p3=p2->link;
+    p1->link=NULL;
+    p2->link=p1;
+    while(p3!=NULL)
+    {
+        p1=p2;
+        p2=p3;
+        p3=p3->link;
+        p2->link=p1;
+    }
+    start=p2;
+}/*End of rev()*/
+//Function to search an element from the linked list
+void Search(int data)
+{
+    struct node *ptr = start;
+    int pos = 1;
+//searching for an element in the linked list
+    while(ptr!=NULL)
+    {
+        if (ptr->info==data)
+        {
+            printf ("\n\nItem %d found at position %d", data, pos);
+            getch();
+            return;
+        }
+        ptr = ptr->link;
+        pos++;
+    }
+    if (ptr == NULL)
+        printf ("\n\nItem %d not found in list",data);
+    getch();
+}
+void main()
+{
+    int choice,n,m,position,i;
+    start=NULL;
+    while(1)
+    {
+        clrscr();
+        printf ("1.Create List\n");
+        printf ("2.Add at beginning\n");
+        printf ("3.Add after \n");
+        printf ("4.Delete\n");
+        printf ("5.Display\n");
+        printf ("6.Count\n");
+        printf ("7.Reverse\n");
+        printf ("8.Search\n");
+        printf ("9.Quit\n");
+        printf ("\nEnter your choice:");
+        scanf ("%d",&choice);
+        switch (choice)
+        {
+        case 1:
+            printf ("\n\nHow many nodes you want:");
+            scanf ("%d",&n);
+            for(i = 0; i<n; i++)
+            {
+                printf ("\nEnter the element:");
+                scanf ("%d",&m);
+                Create_List(m);
+            }
+            break;
+        case 2:
+            printf ("\n\nEnter the element : ");
+            scanf ("%d",&m);
+            AddAtBeg(m);
+            break;
+        case 3:
+            printf ("\n\nEnter the element:");
+            scanf ("%d",&m);
+            printf ("\nEnter the position after which this element is inserted:");
+            scanf ("%d",&position);
+            Add After(m,position);
+            break;
+        case 4:
+            if (start == NULL)
+            {
+                printf("\n\nList is empty");
+                continue;
+            }
+            printf ("\n\nEnter the element for deletion:");
+            scanf ("%d",&m);
+            Del(m);
+            break;
+        case 5:
+            Display();
+            break;
+        case 6:
+            Count();
+            break;
+        case 7:
+            Rev();
+            break;
+        case 8:
+            printf("\n\nEnter the element to be searched:");
+            scanf ("%d",&m);
+Search(m);
+break;
+ case 9:
+exit(0);
+ default:
+printf ("\n\nWrong choice");
+}/*End of switch*/
+}/*End of while*/
+}/*End of main()*/
