46ac2e0cb7106a7b0013f60e3e56f8f34324d177
sajjad-njr <sajjadrahman1020@gmail.com>
2020-12-24 23:17:59 +0600

    linked list  for 385 pdf

--- Z Self Prectice/End of the week/223.linked-list.c
+++ Z Self Prectice/End of the week/223.linked-list.c
@@ -1,37 +1,108 @@
 #include<stdio.h>
-#include<stdlib.h>
-
-typedef struct node
+#include<conio.h>
+#include<malloc.h>
+#include<process.h>
+//Structure declaration for the node
+struct node
 {
-    int data ;
-    struct node *next;
-} Node;
-
-
-int main()
+    int info;
+    struct node *link;
+}*start;
+//This function will create a new linked list
+void Create_List(int data)
 {
-    Node *h1,*h2,*h3;
-
-    h1 = (Node*) malloc (sizeof(Node));
-    h2 = (Node*) malloc (sizeof(Node));
-    h3 = (Node*) malloc (sizeof(Node));
-
-    h1->data = 1;
-    h1->next = h2;
-
-    h2->data = 100;
-    h2->next = h3;
-
-    h3->data = 19700;
-    h3->next = NULL;
-
-    printf("\n%u H1 Node value is = %d \tNode Address = %u",h1,h1->data,h1->next);
-
-    printf("\n%u H2 Node value is = %d \tNode Address = %u",h2,h2->data,h2->next);
-
-    printf("\n%u H3 Node value is = %d \tNode Address = %u",h3,h3->data,h3->next);
-
-    return 0;
-
-}
-
+    struct node *q,*tmp;
+//Dynamic memory is been allocated for a node
+    tmp= (struct node*)malloc(sizeof(struct node));
+    tmp->info=data;
+    tmp->link=NULL;
+    if(start==NULL) /*If list is empty*/
+        start=tmp;
+    else
+    {
+        /*Element inserted at the end*/
+        q=start;
+        while(q->link!=NULL)
+            q=q->link;
+        q->link=tmp;
+    }
+}/*End of create_list()*/
+//This function will add new element at the beginning of the linked list
+void AddAtBeg(int data)
+{
+    struct node *tmp;
+    tmp=(struct node*)malloc(sizeof(struct node));
+    tmp->info=data;
+    tmp->link=start;
+    start=tmp;
+}/*End of addatbeg()*/
+//Following function will add new element at any position
+void AddAfter(int data,int pos)
+{
+    struct node *tmp,*q;
+    int i;
+    q=start;
+//Finding the position to add new element to the linked list
+    for(i=0; i<pos-1; i++)
+    {
+        q=q->link;
+        if(q==NULL)
+        {
+            printf ("\n\n There are less than %d elements",pos);
+            getch();
+            return;
+        }
+    }/*End of for*/
+    tmp=(struct node*)malloc(sizeof (struct node));
+    tmp->link=q->link;
+    tmp->info=data;
+    q->link=tmp;
+}/*End of addafter()*/
+//Delete any element from the linked list
+void Del(int data)
+{
+    struct node *tmp,*q;
+    if (start->info == data)
+    {
+        tmp=start;
+        start=start->link; /*First element deleted*/
+        free(tmp);
+        return;
+    }
+    q=start;
+    while(q->link->link ! = NULL)
+    {
+        if(q->link->info == data) /*Element deleted in between*/
+        {
+            tmp=q->link;
+            q->link=tmp->link;
+            free(tmp);
+            return;
+        }
+        q=q->link;
+    }/*End of while */
+    if(q->link->info==data) /*Last element deleted*/
+    {
+        tmp=q->link;
+        free(tmp);
+        q->link=NULL;
+        return;
+    }
+    printf ("\n\nElement %d not found",data);
+    getch();
+}/*End of del()*/
+//This function will display all the element(s) in the linked list
+void Display()
+{
+    struct node *q;
+    if(start == NULL)
+    {
+        printf ("\n\nList is empty");
+        return;
+    }
+    q=start;
+    printf("\n\nList is : ");
+    while(q!=NULL)
+    {
+        printf ("%d ", q->info);
+        q=q->link;
