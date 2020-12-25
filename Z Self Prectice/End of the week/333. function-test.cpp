df7d620353a5c22c91fe52967d7fd9d8f00c24b7
sajjad-njr <sajjadrahman1020@gmail.com>
2020-12-25 13:58:21 +0600

    sum

--- Z Self Prectice/End of the week/333. function-test.cpp
+++ Z Self Prectice/End of the week/333. function-test.cpp
@@ -1,10 +1,15 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+void sum1(int a = 100 , int b = 90 , int c = 10)
+{
+    return a + b + c ;
+}
 int main()
 {
     int a = 8 ; int b = 9 ;
     int sum = a + b;
+    cout << sum1(a,b) << endl;
     cout << sum ;
     return 0;
 }
