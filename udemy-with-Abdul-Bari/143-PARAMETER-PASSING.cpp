6eb13e3a3245e086b79243437794ee7b4aece44e
sajjad-njr <sajjadrahman1020@gmail.com>
2020-12-25 22:19:00 +0600

    passing parameter

--- udemy-with-Abdul-Bari/143-PARAMETER-PASSING.cpp
+++ udemy-with-Abdul-Bari/143-PARAMETER-PASSING.cpp
@@ -1,11 +1,20 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void swap(int *a , int *b)
+void swap(int *a, int *b)
 {
     int temp;
     temp = *a;
     *a = *b;
     *b = temp;
 
+}
+
+int main()
+{
+    int ab = 100, bb = 200;
+
+    swap(&ab, &bb);
+    cout << ab << " " << bb ;
+    return 0;
 }
