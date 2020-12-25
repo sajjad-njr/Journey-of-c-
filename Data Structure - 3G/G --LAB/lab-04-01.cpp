b2ac82018d2a8ba8f6a68b8650b961b7af4ff329
sajjad-njr <sajjadrahman1020@gmail.com>
2020-12-25 23:17:16 +0600

    struct with pitiraz

--- Data Structure - 3G/G --LAB/lab-04-01.cpp
+++ Data Structure - 3G/G --LAB/lab-04-01.cpp
@@ -10,10 +10,12 @@ struct student
 
 int main()
 {
+    /* for 1 student
     student info ;
     cin.get(info.name,50);
     cin>> info.id;
     cin >> info.marks;
+    */
 
     cout << info.name << " " << info.id << " " << info.marks  << endl;
     return 0;
