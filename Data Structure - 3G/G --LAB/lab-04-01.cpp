301736b2e1478f3f2c61d633dedaba63079680ea
sajjad-njr <sajjadrahman1020@gmail.com>
2020-12-25 23:23:47 +0600

    struct with pitiraz

--- Data Structure - 3G/G --LAB/lab-04-01.cpp
+++ Data Structure - 3G/G --LAB/lab-04-01.cpp
@@ -3,20 +3,39 @@ using namespace std ;
 
 struct student
 {
-    char name[50];
+     string name;
     int id ;
     float marks ;
 };
 
 int main()
 {
+    student info[5] ;
+    int i;
     /* for 1 student
     student info ;
     cin.get(info.name,50);
     cin>> info.id;
     cin >> info.marks;
-    */
 
-    cout << info.name << " " << info.id << " " << info.marks  << endl;
+     cout << info.name << " " << info.id << " " << info.marks  << endl;
+
+    */
+    // for multiple student
+
+    for(i = 0 ; i < 5 ; i++)
+    {
+        //cin.get(info[i].name,50);
+        cin >> name;
+        cin>> info[i].id;
+        cin >> info[i].marks;
+    }
+
+    for (i = 0 ; i < 5 ; i++)
+    {
+        cout << info[i].name << " " << info[i].id << " " << info[i].marks  << endl;
+    }
+
+
     return 0;
 }
