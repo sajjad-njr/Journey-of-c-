72ff1fa4870712ac0778dc1e387371016b9ab455
sajjad-njr <sajjadrahman1020@gmail.com>
2020-12-25 22:46:54 +0600

    passing parameter

--- Z Self Prectice/End of the week/444.insert-linked-list.c
+++ Z Self Prectice/End of the week/444.insert-linked-list.c
@@ -1,8 +1,16 @@
 #include<stdio.h>
 #include<stdlib.h>
-struct node
+typedef struct node
 {
- int data;
- struct node *next;
-};
+    int data;
+    struct node *next;
+} nfo;
 
+void insert_front( nfo *list,int data)
+{
+    nfo *temp;
+    temp=(nfo*)malloc(n*sizeof(nfo));
+    temp->data=data;
+    temp-next=list;
+    list=temp;
+}
