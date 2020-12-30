d4c4e07f51a5933dff112844bced09189d738d5d
sajjad-njr <sajjadrahman1020@gmail.com>
2020-12-30 22:52:16 +0600

    linked list create

--- Data Structure - 3G/Greek for Geeks/1.linked-list-create.c
+++ Data Structure - 3G/Greek for Geeks/1.linked-list-create.c
@@ -4,8 +4,8 @@
 typedef struct node
 {
     int data ;
-    struct node *next
-    } Node;
+    struct node *next;
+} Node;
 
 
 int main()
@@ -14,8 +14,21 @@ int main()
     Node  *second = NULL;
     Node *third = NULL;
 
-    head =(Node*) malloc(sizeof(Node));
-    second=(Node*) malloc(sizeof(Node));
-    third   =(Node*) malloc(sizeof(Node));
+    head = (Node*) malloc(sizeof(Node));
+    second = (Node*) malloc(sizeof(Node));
+    third  =(Node*) malloc(sizeof(Node));
+
+    head->data = 1 ;
+    //scanf("%d",&head->data);
+    head ->next = second;
+
+    second ->data = 2 ;
+    second ->next = third;
+
+    third ->data = 3 ;
+    third ->next = NULL;
+
+    printf("\n\tis 1st node value = %d ",head->data);
+    return 0;
 
 }
