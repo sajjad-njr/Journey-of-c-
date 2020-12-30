5bac39c96c084e6b962c459f37385686f9be22fb
sajjad-njr <sajjadrahman1020@gmail.com>
2020-12-31 00:16:33 +0600

    traversing  linked list

--- Data Structure - 3G/Greek for Geeks/2.treaversing-linked-list.c
+++ Data Structure - 3G/Greek for Geeks/2.treaversing-linked-list.c
@@ -1,22 +1,40 @@
- #include<stdio.h>
- #include<stdlib.h>
+#include<stdio.h>
+#include<stdlib.h>
 
- typedef struct node()
- {
-     int data;
-     struct node *next ;
- }Node;
+typedef struct node
+{
+    int data;
+    struct node *next ;
+}
+Node;
 
- void printList(Node n)
- {
-     while(n != NULL)
-     {
-         printf(" %d ",n->data);
-         n = n -> next ;
-     }
- }
+void printList(Node* n)
+{
+    while(n != NULL)
+    {
+        //if(n->data == 456)
+        //if(n->next == NULL)
+        printf(" %d ",n->data);
 
- int main()
- {
+        n = n -> next ;
+    }
+}
 
- }
+int main()
+{
+    Node *head = NULL;
+    Node *second = NULL;
+
+    head = (Node*) malloc(sizeof(Node));
+    second = (Node*) malloc(sizeof(Node));
+
+    head->data = 456;
+    head ->next = second ;
+
+    second -> data = 111;
+    second -> next = NULL;
+
+    printList(head);
+    return 0;
+
+}
