d9760cb4e8b1fe513361462f6be3b12a5ad2d811
sajjad-njr <sajjadrahman1020@gmail.com>
2020-12-31 19:41:39 +0600

    linked  insert element in fornt

--- Data Structure - 3G/Greek for Geeks/9999.3.c
+++ Data Structure - 3G/Greek for Geeks/9999.3.c
@@ -9,24 +9,32 @@ typedef struct node
 
 void push(Node** head_add, int n )
 {
-
+    Node *new_node = (Node*)malloc(sizeof(Node));
+    new_node -> data = n ;
+    new_node -> next = (*head_add);
+    (*head_add ) = new_node;
 }
 
 int main()
 {
-    Node *head, *second, *third ;
+    Node *head, *first,  *second, *third ;
     head = (Node*) malloc(sizeof(Node));
     first = (Node*) malloc(sizeof(Node));
     second  = (Node*) malloc(sizeof(Node));
 
     head ->data = 374;
-    head ->next = second;
+    head ->next =  first;
 
     first ->data = 89;
-    first ->next = third ;
+    first ->next =  second ;
 
     second -> data = 46;
     second ->next = NULL;
+    int r = 909090;
+
+    //insert data for 1st position ;
+    push(&head,r);
+    push(&head,3030);
 
     while(head != NULL)
     {
