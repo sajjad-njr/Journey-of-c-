5be2035cbae1538d43eda3788ae20c4a7778cff2
sajjad-njr <sajjadrahman1020@gmail.com>
2021-01-01 15:00:06 +0600

    FLOW002

---
+++ OJ/code chef/12.FLOW002.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std ;
+
+int main()
+{
+    int a , b ;
+
+    int t ;
+
+    cin >> t ;
+
+    while ( t--)
+    {
+        cin >> a >> b ;
+        cout << a%b << endl;
+    }
+
+    return 0;
+}
