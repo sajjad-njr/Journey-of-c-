dc1dcd12e79ab95c4c5f3b8c152b90a1af0866e4
sajjad-njr <sajjadrahman1020@gmail.com>
2021-01-01 21:40:46 +0600

    linked list

--- Z Self Prectice/1-1-2021/1..c
+++ Z Self Prectice/1-1-2021/1..c
@@ -1,1 +1,37 @@
+#include<stdio.h>
+#include<stdlib.h>
 
+typedef struct node
+{
+    int data ;
+    struct node *next ;
+
+} Node;
+
+//void new_data();
+
+int main()
+{
+    Node *a, *b, *c, *d;
+
+    a = (Node*) malloc (sizeof(Node));
+    b = (Node*) malloc (sizeof(Node));
+    c = (Node*) malloc  (sizeof(Node));
+
+    a->data = 56;
+    a->next = b ;
+
+    b->data = 567;
+    b->next = c ;
+
+    c->data = 70;
+    c->next = NULL ;
+
+    while(a != NULL)
+    {
+        prinf(" %d ", a->data);
+        a = a ->next ;
+    }
+
+    return 0;
+}
