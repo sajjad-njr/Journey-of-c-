f114a9ecbf8938d58d0c2665739b365a8273ad43
sajjad-njr <sajjadrahman1020@gmail.com>
2021-01-03 18:48:25 +0600

    node insert

--- Z Self Prectice/3-1-2021/1.create-node.c
+++ Z Self Prectice/3-1-2021/1.create-node.c
@@ -12,4 +12,33 @@ int main()
 {
     Node *head , *temp, *new_node;
 
+    head = NULL;
+
+    int n = 5 ;
+
+    while (n--)
+    {
+        new_node = (Node*)malloc(sizeof(Node));
+        printf("\n\tEnter a Node = ");
+        scanf("%d",&new_node->data);
+        new_node->next = NULL;
+
+        if(head == NULL)
+        {
+            head = temp = new_node ;
+        }
+        else
+        {
+            temp->next = new_node ;
+            temp = new_node;
+        }
+    }
+    temp = head;
+
+    while( temp != NULL)
+    {
+        printf("  %d  ",temp->data);
+        temp = temp ->next ;
+    }
+    return 0;
 }
