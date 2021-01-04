40f1a481d44dcfa1f3769fbef616c7d9d7287546
sajjad-njr <sajjadrahman1020@gmail.com>
2021-01-04 15:16:27 +0600

    from delete  start

--- Z Self Prectice/1-1-2021/7.delete-from-start.c
+++ Z Self Prectice/1-1-2021/7.delete-from-start.c
@@ -6,8 +6,41 @@ typedef struct node
     int data;
     struct node *next;
 }Node;
+Node *head, *temp , *new_node;
 
+void create()
+{
+    int n = 4;
+    head = NULL;
+    while(n--)
+    {
+        new_node = (Node*)malloc(sizeof(Node));
+        printf("\n\tenter node = ");
+        scanf("%d",&new_node->data);
+        new_node->next = NULL;
+
+        if(head == NULL)
+        {
+            head = temp = new_node;
+        }
+        else
+        {
+            temp->next = new_node;
+            temp = new_node;
+        }
+
+    }
+}
 int main()
 {
+    //Node *head, *temp;
+    temp = head;
 
+    create();
+
+    while(temp != NULL)
+    {
+        printf(" %d ",temp->data);
+        temp = temp ->next;
+    }
 }
