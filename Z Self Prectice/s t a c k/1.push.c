f844b7e145f494ffd46fd953b012bb9aa4c79b63
sajjad-njr <sajjadrahman1020@gmail.com>
2021-01-05 21:54:59 +0600

    stack push

---
+++ Z Self Prectice/s t a c k/1.push.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+#define N 5;
+int stack[N];
+int top = -1 ;
+void push()
+{
+    int a;
+    printf("\n\tEnter the value = ");
+    scanf("%d",&a);
+
+    if(top == N-1)
+    {
+        printf("\n\tOverflow");
+
+    }
+    else
+    {
+        top++;
+        stack[top] = x;
+    }
+
+}
+
+void
