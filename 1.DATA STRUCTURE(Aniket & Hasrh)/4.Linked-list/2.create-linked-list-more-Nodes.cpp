398245484edfe524a5a74ad372e6b80e9ae4edee
sajjad-njr <sajjadrahman1020@gmail.com>
2021-01-07 20:00:35 +0600

    linked list  with more nodes

--- 1.DATA STRUCTURE(Aniket & Hasrh)/4.Linked-list/2.create-linked-list-more-Nodes.cpp
+++ 1.DATA STRUCTURE(Aniket & Hasrh)/4.Linked-list/2.create-linked-list-more-Nodes.cpp
@@ -14,12 +14,35 @@ public:
     }
 };
 
-void print(*head)
+void print( Node *head)
 {
     while(head != NULL)
     {
-        cout << head->data << " ";
+        cout << head->data << " -> ";
         head = head ->next ;
     }
     cout<<"NULL";
+}
+
+int main()
+{
+    Node n1(2);
+    Node *head = &n1;
+
+    Node n2(3);
+    Node n3(13);
+    Node n4(23);
+    Node n5(33);
+    Node n6(43);
+    Node n7(53);
+
+    n1.next=&n2;
+    n2.next=&n3;
+    n3.next=&n4;
+    n4.next=&n5;
+    n5.next=&n6;
+    n6.next=&n7;
+
+    print(head);
+    return 0;
 }
