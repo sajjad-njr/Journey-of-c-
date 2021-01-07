c19588ef08977dd62056a30f128c0edbf5d1441d
sajjad-njr <sajjadrahman1020@gmail.com>
2021-01-07 20:07:46 +0600

    takeInput means user create node

--- 1.DATA STRUCTURE(Aniket & Hasrh)/4.Linked-list/3.takeInput.cpp
+++ 1.DATA STRUCTURE(Aniket & Hasrh)/4.Linked-list/3.takeInput.cpp
@@ -14,8 +14,41 @@ public:
     }
 };
 
-Node takeInput()]
+Node takeInput()
 {
     int data;
     cin>>data;
+
+    Node *head = NULL;
+    Node *tail = NULL;
+
+    while(data != -1)
+    {
+        Node *n = new Node (data);
+
+        if(head == NULL)
+        {
+            head = n ;
+            tail = n ;
+        }
+        else
+        {
+            tail->next = n ;
+            tail = n ;
+
+        }
+        cin>>data;
+    }
+    return head ;
 }
+
+void print( Node *head)
+{
+    while(head != NULL)
+    {
+        cout << head->data << " -> ";
+        head = head ->next ;
+    }
+    cout<<"NULL";
+}
+
