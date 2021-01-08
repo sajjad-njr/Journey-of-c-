23005bc22baa2acdb9e2673d7a0c77cd66b2e1ee
sajjad-njr <sajjadrahman1020@gmail.com>
2021-01-09 04:00:05 +0600

    inset at first position

--- Z Self Prectice/1. W A S S I/2.insert-AT-FIRST-position.c
+++ Z Self Prectice/1. W A S S I/2.insert-AT-FIRST-position.c
@@ -12,8 +12,15 @@ int main()
     Node *head , *new_node , *temp ;
     Node *n1,*n2;
 
+    head =   (Node*)malloc(sizeof(Node));
     n1 = (Node*)malloc(sizeof(Node));
     n2 = (Node*)malloc(sizeof(Node));
+    new_node = (Node*)malloc(sizeof(Node));
+
+    printf("\n\tNew data ");
+    scanf("%d",&new_node->data);
+    new_node->next = head;
+    head = new_node;
 
     head = n1;
     n1->data = 2 ;
@@ -22,11 +29,13 @@ int main()
     n2->data = 3;
     n2->next = NULL;
 
+    temp = head ;
 
-    while(head != NULL)
+
+    while( temp != NULL)
     {
-        printf(" %d ",head->data);
-        head = head->next;
+        printf(" %d ",temp->data);
+        temp = temp->next;
     }
 
 }
