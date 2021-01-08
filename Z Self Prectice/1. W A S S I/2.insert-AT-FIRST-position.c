9a99c25106f3e5e22a81870906dc80b4decf05c8
sajjad-njr <sajjadrahman1020@gmail.com>
2021-01-09 03:46:46 +0600

    inset at first position

--- Z Self Prectice/1. W A S S I/2.insert-AT-FIRST-position.c
+++ Z Self Prectice/1. W A S S I/2.insert-AT-FIRST-position.c
@@ -9,5 +9,19 @@ typedef struct node
 
 int main()
 {
+    Node *head , *new_node , *temp ;
+    Node *n1,*n2;
+
+    n1 = (Node*)malloc(sizeof(Node));
+    n2 = (Node*)malloc(sizeof(Node));
+
+    n1->data = 2 ;
+    n1->next = n2;
+
+    n2->data = 3;
+    n2->next = NULL;
+
+
+
 
 }
