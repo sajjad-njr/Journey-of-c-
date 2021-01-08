6f55205c4eff0699a506b5ff6626c8affee729de
sajjad-njr <sajjadrahman1020@gmail.com>
2021-01-08 22:39:51 +0600

    create linked list

--- Z Self Prectice/8-01-2021/1.create-linked-list.c
+++ Z Self Prectice/8-01-2021/1.create-linked-list.c
@@ -1,9 +1,18 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+typedef struct node
+{
+   int data ;
+   struct node *next;
+
+}Node;
 //create linked list ;
 
+void createNode()
+{
 
+}
 int main()
 {
 
