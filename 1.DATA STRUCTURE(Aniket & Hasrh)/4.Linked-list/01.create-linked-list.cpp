d163234e96806e351d48b809d04d9de13cc14f3d
sajjad-njr <sajjadrahman1020@gmail.com>
2021-01-11 22:54:47 +0600

    create linked list

--- 1.DATA STRUCTURE(Aniket & Hasrh)/4.Linked-list/01.create-linked-list.cpp
+++ 1.DATA STRUCTURE(Aniket & Hasrh)/4.Linked-list/01.create-linked-list.cpp
@@ -14,3 +14,19 @@ public:
         next = NULL;
     }
 };
+
+int  main()
+{
+    //statistically
+    Node n1(1);
+    Node n2(3);
+
+    n1.next = &n2;
+
+    Node *head = &n1;
+
+    cout << head->data << endl;
+
+    cout << n1.data << " " << n2.data << endl;
+    return 0;
+}
