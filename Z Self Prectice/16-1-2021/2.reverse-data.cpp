213414e7d6f62f90a681b5d8ec8580e688014f06
sajjad-njr <sajjadrahman1020@gmail.com>
2021-01-16 21:55:33 +0600

    reverse output

---
+++ Z Self Prectice/16-1-2021/2.reverse-data.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int n;
+    Node *next;
+
+    Node(int n)
+    {
+        this->n = n;
+        next = NULL;
+    }
+};
