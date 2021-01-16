2089b3f844c9a19153ce6c72cca528bcd42e5f6b
sajjad-njr <sajjadrahman1020@gmail.com>
2021-01-16 21:31:25 +0600

    create node

---
+++ Z Self Prectice/16-1-2021/1.create-linked-list.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+    int data ;
+    Node *next;
+
+    Node(int data)
+    {
+        this->data = data;
+        next = NULL;
+    }
+};
+int main()
+{
+
+}
