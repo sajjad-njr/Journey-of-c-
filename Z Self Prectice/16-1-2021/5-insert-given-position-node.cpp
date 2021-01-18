8877e34a4656da70a5956167a3be7fbd31d5a96b
sajjad-njr <sajjadrahman1020@gmail.com>
2021-01-18 09:12:18 +0600

    isert given position

--- Z Self Prectice/16-1-2021/5-insert-given-position-node.cpp
+++ Z Self Prectice/16-1-2021/5-insert-given-position-node.cpp
@@ -13,3 +13,40 @@ public:
         next = NULL;
     }
 };
+
+Node* takeInput()
+{
+    Node *head , *tail;
+    head = NULL;
+    tail = NULL;
+
+    int val; //val = value
+    cin>>val;
+    while(val != -1)
+    {
+        Node *new_node = new Node(val)
+         if(head == NULL)
+         {
+             head = new_node;
+             tail= new_node;
+         }
+         else
+         {
+             tail->next = new_node;
+             tail = new_node;
+         }
+         cin >> val;
+    }
+    return head ;
+}
+
+void output(Node *h)
+{
+    while(h !=  NULL)
+    {
+        cout<< h->data << " -> ";
+        h =h->next;
+    }
+    cout<<"NULL" << endl;
+}
+
