fad7c5f33e2cbc2c9734c5fb4b978cf785dd96a7
sajjad-njr <sajjadrahman1020@gmail.com>
2021-01-18 22:53:45 +0600

    insert postion

--- Z Self Prectice/1.Lab-preparation/Array/2.insert-position.c
+++ Z Self Prectice/1.Lab-preparation/Array/2.insert-position.c
@@ -1,1 +1,37 @@
+#include<stdio.h>
+
+int main()
+{
+    int size, position, value ;
+    printf("\nEnter the size of array = ");
+    scanf("%d",&size);
+
+    int a[size],i;
+    printf("\nEnter the size of array = ");
+
+    for(i = 0 ; i < size ; i++)
+    {
+        scanf("%d",&a[i]);
+    }
+
+    printf("\nEnter the value that insert = ");
+    scanf("%d",&value);
+
+    printf("\nEnter the position = ");
+    scanf("%d",&position);
+
+    for(int i = size-1 ; i >= position-1 ; i--)
+    {
+        a[i+1]= a[i];
+
+    }
+    a[position-1] = value;
+    size++;
+
+    for(i = 0 ; i < size ; i++)
+    {
+        printf(" %d ",a[i]);  // if say add 5 all value (%d + 5);
+    }
+    return 0;
+}
 
