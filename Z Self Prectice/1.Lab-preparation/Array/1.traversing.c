4245ad54b7eef2867fc149769b958d758532d97b
sajjad-njr <sajjadrahman1020@gmail.com>
2021-01-18 22:41:19 +0600

    traversing array

--- Z Self Prectice/1.Lab-preparation/Array/1.traversing.c
+++ Z Self Prectice/1.Lab-preparation/Array/1.traversing.c
@@ -3,11 +3,22 @@
 int main()
 {
     int size;
-    printf("Enter the size of array = ");
+    printf("\nEnter the size of array = ");
     scanf("%d",&size);
 
     int a[size],i;
-    printf("Enter the size of array = ");
+    printf("\nEnter the size of array = ");
 
+    for(i = 0 ; i < size ; i++)
+    {
+        scanf("%d",&a[i]);
+    }
 
+     printf("\nArray elements = ");
+
+    for(i = 0 ; i < size ; i++)
+    {
+        printf(" %d ",a[i]);
+    }
+    return 0;
 }
