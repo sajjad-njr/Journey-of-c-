de04bfd3a0640fd4b9c8fb1f6367fc0e0ddf61df
sajjad-njr <sajjadrahman1020@gmail.com>
2021-01-19 10:07:48 +0600

    read and print string

---
+++ Z Self Prectice/1.Lab-preparation/2. String/1.rea-print-string.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char str[20];
+    printf("\nEnter your name = ");
+    gets(str);
+
+    printf("\nyour name is = ");
+    puts(str);
+
+    return 0;
+}
