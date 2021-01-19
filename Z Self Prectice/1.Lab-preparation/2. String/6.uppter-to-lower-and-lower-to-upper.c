654103173fab19f624a84a26cd47cc80dd41b267
sajjad-njr <sajjadrahman1020@gmail.com>
2021-01-19 16:37:52 +0600

    upper to lower

---
+++ Z Self Prectice/1.Lab-preparation/2. String/6.uppter-to-lower-and-lower-to-upper.c
@@ -0,0 +1,33 @@
+//upper to lower && lower to upper
+
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+    char str[100];
+    int i;
+
+    printf("\n\tEnter string = ");
+    gets(str);
+
+    for(i=0 ; str[i] != '\0' ; i++)
+    {
+        //upper to lower convert
+        if(str[i] >= 'A' && str[i]<= 'Z')
+        {
+            str[i] = str[i] + 32;
+        }
+
+        //lower to upper
+        else
+        {
+            if(str[i] >= 'a' && str[i]<= 'z')
+            {
+                str[i] = str[i] - 32;
+            }
+        }
+    }
+    printf("New string = %s",str);
+    return 0;
+}
