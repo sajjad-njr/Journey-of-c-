5d3f96358d201fb5bc5e20290f3cf32e927f7051
sajjad-njr <sajjadrahman1020@gmail.com>
2021-01-19 18:24:11 +0600

    vowel and consonant

--- Z Self Prectice/1.Lab-preparation/2. String/7.vowel-consonate.c
+++ Z Self Prectice/1.Lab-preparation/2. String/7.vowel-consonate.c
@@ -6,29 +6,33 @@
 int main()
 {
     char str[100];
-    int i;
+    int i, length = 0, vowel = 0;
 
     printf("\n\tEnter string = ");
     gets(str);
 
     for(i=0 ; str[i] != '\0' ; i++)
     {
-        //upper to lower convert
-        if(str[i] >= 'A' && str[i]<= 'Z')
+
+        length++;
+
+        if(str[i] == 'A' || str[i] == 'E' || str[i] == 'I' || str[i] =='O' ||  str[i] == 'U')
         {
-            str[i] = str[i] + 32;
+            vowel++;
         }
 
-        //lower to upper
-        else
+        if(str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' ||  str[i] == 'u')
         {
-            if(str[i] >= 'a' && str[i]<= 'z')
-            {
-                str[i] = str[i] - 32;
-            }
+            vowel++;
         }
+
+
     }
-    printf("\n\tNew string = %s",str);
+    printf("\n\tVowel = %d",vowel);
+
+    printf("\n\tConsonant = %d",length-vowel);
+
+
     return 0;
 }
 
