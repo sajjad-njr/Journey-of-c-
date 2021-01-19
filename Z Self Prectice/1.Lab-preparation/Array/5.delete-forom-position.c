dbdda6b32cbac38ac191382e630cb76d9f022a99
sajjad-njr <sajjadrahman1020@gmail.com>
2021-01-19 09:53:20 +0600

    delete element from position

---
+++ Z Self Prectice/1.Lab-preparation/Array/5.delete-forom-position.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+
+int main()
+{
+    int size, position, value ;
+    printf("\n\tEnter the size of array = ");
+    scanf("%d",&size);
+
+    int a[size],i;
+
+
+    for(i = 0 ; i < size ; i++)
+    {
+        scanf("%d",&a[i]);
+    }
+
+    printf("\n\tEnter the position that delete = ");
+    scanf("%d",&position);
+
+    printf("\n\tAfter  delete data new array = ");
+
+    if(position <= 0 || position > size)
+    {
+        printf("\nInvalid Position ");
+    }
+    else
+    {
+        for(i = position - 1  ; i < size -1  ; i++)
+        {
+             a[i] = a[i+1];  //  after position data move 1 position
+        }
+        size--;
+    }
+
+    for(int j = 0 ; j < size ; j++)
+    {
+        printf(" %d ",a[j]);
+    }
+
+    return 0;
+}
+
+
+
+
