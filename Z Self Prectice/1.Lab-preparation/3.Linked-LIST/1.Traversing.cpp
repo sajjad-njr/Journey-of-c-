8e74958f5e83209f6d13bd53e6f32fbf891625ca
sajjad-njr <sajjadrahman1020@gmail.com>
2021-01-20 21:29:13 +0600

    traversing linked list

---
+++ Z Self Prectice/1.Lab-preparation/3.Linked-LIST/1.Traversing.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+    int data;
+    Node *next;
+
+    Node(int data)
+    {
+        this->data = data;
+        next = NULL;
+    }
+};
+
+Node* createNode()
+{
+    Node *head , *tail;
+    head = NULL;
+    tail = NUll;
+
+    int num;
+    cin>>num
+
+}
