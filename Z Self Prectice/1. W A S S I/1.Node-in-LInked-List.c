4fb8e73b529a3bf3623f27de18637985f5857497
sajjad-njr <sajjadrahman1020@gmail.com>
2021-01-21 23:26:44 +0600

    Node create

--- Z Self Prectice/1. W A S S I/1.Node-in-LInked-List.c
+++ Z Self Prectice/1. W A S S I/1.Node-in-LInked-List.c
@@ -13,8 +13,30 @@ Node* create()
     Node *head , *tail;
     head = NULL;
     tail = NULL;
+    int num;
+    scanf("%d",&num);
+
+    while(num--)
+    {
+        Node *new_node = (Node*)malloc(sizeof(Node));
+        printf("\n\tEnter Node value = ");
+        scanf("%d",&new_node->data);
+        new_node->next = NULL;
+
+        if(head == NULL)
+        {
+            head = new_node;
+            tail = new_node;
+
+        }
+        else
+        {
+            tail->next = new_node;
+        }
+    }
+    return head;
 }
 int main()
 {
-
+    create();
 }
