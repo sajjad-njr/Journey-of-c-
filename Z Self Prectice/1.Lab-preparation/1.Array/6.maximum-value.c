d1378bbf2785fe33caa3dd1cea7e1a54745b765f
sajjad-njr <sajjadrahman1020@gmail.com>
2021-01-21 23:04:58 +0600

    maximum value in arrau

--- Z Self Prectice/1.Lab-preparation/1.Array/6.maximum-value.c
+++ Z Self Prectice/1.Lab-preparation/1.Array/6.maximum-value.c
@@ -1,1 +1,20 @@
+#include<stdio.h>
 
+int main()
+{
+    int size;
+    printf("\n\tEnter the size of array = ");
+    scanf("%d",&size);
+
+    int  DATA[size];
+
+    printf("\n\tEnter the value of array = ");
+
+    for(int i = 0 ; i < size ; i++)
+    {
+        scanf("&d",&DATA[i]);
+    }
+
+    int max = DATA[0];
+
+}
