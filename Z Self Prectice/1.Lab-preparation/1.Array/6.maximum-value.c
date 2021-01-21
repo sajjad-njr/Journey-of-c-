0eedf26a615d24d4ca3ec929cfebc5ac73b597f5
sajjad-njr <sajjadrahman1020@gmail.com>
2021-01-21 23:09:57 +0600

    maximum value in arrau

--- Z Self Prectice/1.Lab-preparation/1.Array/6.maximum-value.c
+++ Z Self Prectice/1.Lab-preparation/1.Array/6.maximum-value.c
@@ -6,15 +6,29 @@ int main()
     printf("\n\tEnter the size of array = ");
     scanf("%d",&size);
 
-    int  DATA[size];
+    int  DATA[size] , pos = 0;
 
     printf("\n\tEnter the value of array = ");
 
     for(int i = 0 ; i < size ; i++)
     {
-        scanf("&d",&DATA[i]);
+        scanf("%d",&DATA[i]);
     }
 
     int max = DATA[0];
+
+    for(int i = 0 ; i < size ; i++)
+    {
+         if(max < DATA[i])
+         {
+             max = DATA[i];
+             pos = i ;
+         }
+    }
+    printf("\n\tMaximum value is = %d",max);
+
+    printf("\n\tMaximum value is find = %d",pos+1);
+
+    return 0;
 
 }
