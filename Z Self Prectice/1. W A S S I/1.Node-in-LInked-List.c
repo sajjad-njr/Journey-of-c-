4b18c47e122a40a84217f38ca55dc6232a2ec263
sajjad-njr <sajjadrahman1020@gmail.com>
2021-01-21 23:22:29 +0600

    Node create

---
+++ Z Self Prectice/1. W A S S I/1.Node-in-LInked-List.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct node
+{
+    int data;
+    struct node *next;
+
+}Node;
+
+Node* create()
+{
+    Node *head , *tail;
+    head = NULL;
+    tail = NULL;
+}
+int main()
+{
+
+}
