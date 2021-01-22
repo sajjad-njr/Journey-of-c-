ed0683c6ed61a331bc70f6e6129c494d90a3e9c6
sajjad-njr <sajjadrahman1020@gmail.com>
2021-01-22 18:02:48 +0600

    Push on stack

--- Z Self Prectice/1.Lab-preparation/4. S T A C K/1. Array/1.Push.c
+++ Z Self Prectice/1.Lab-preparation/4. S T A C K/1. Array/1.Push.c
@@ -1,8 +1,19 @@
 #include<stdio.h>
 #include<stdlib.h>
 #define N 5
-int stack[N], top = -1;
+int stack[N], Top = -1;
 
+void push()
+{
+    int data;
+    printf("\n\tEnter data = ");
+    scnaf("%d",&data);
+
+    if(Top == N)
+    {
+        printf("\n\tOverflow");
+    }
+}
 int main()
 {
 
