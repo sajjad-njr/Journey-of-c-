d69045417977f788355c3b0ae46f078bdc785f35
sajjad-njr <sajjadrahman1020@gmail.com>
2021-01-22 18:12:06 +0600

    Push on stack

--- Z Self Prectice/1.Lab-preparation/4. S T A C K/1. Array/1.Push.c
+++ Z Self Prectice/1.Lab-preparation/4. S T A C K/1. Array/1.Push.c
@@ -7,14 +7,39 @@ void push()
 {
     int data;
     printf("\n\tEnter data = ");
-    scnaf("%d",&data);
+    scanf("%d",&data);
 
-    if(Top == N)
+    if(Top == N - 1)
     {
         printf("\n\tOverflow");
     }
+    else
+    {
+        Top++;
+        stack[Top] = data;
+    }
 }
-int main()
+void display()
 {
 
+
+
+    int i ;
+    for(i = Top ; i >=0 ; i--)
+    {
+        printf(" %d ",stack[i]);
+    }
+
+}
+
+int main()
+{
+    int n ;
+    scanf("%d",&n);
+
+    while(n--)
+    {
+        push();
+    }
+    display();
 }
