aefa17a3eb33d0cf8322aa1a4de76d2e2c3548ca
sajjad-njr <sajjadrahman1020@gmail.com>
2021-01-23 22:57:02 +0600

    first pattern matching

--- Z Self Prectice/16-12-2020/3.first-pattern-maticng.c
+++ Z Self Prectice/16-12-2020/3.first-pattern-maticng.c
@@ -4,29 +4,29 @@
 int main()
 {
     char str1[100],str2[100];
-    int str1_len , str2_len;
+    int str1_len, str2_len;
 
     gets(str1);
     gets(str2);
     str1_len = strlen(str1);
     str2_len = strlen(str2);
 
-    int  l , k = 0, max = str1_len - str2_len ;
+    int  l, k = 0, max = str1_len - str2_len ;
 
     while(k <= max)
     {
-       for(l = 0 ; l < str1_len ; l++)
-       {
-           if(str2[l] != str1[k + l - 1])
-           {
-               k++;
-               break;
-           }
-       }
-       if(l == str2_len )
-       {
-
-       }
+        for(l = 0 ; l < str1_len ; l++)
+        {
+            if(str2[l] != str1[k + l - 1])
+            {
+                k++;
+                break;
+            }
+        }
+        if(l == str2_len )
+        {
+            printf("\n\tThe patter is match = %d",k);
+        }
     }
 
     printf(" %d %d ",str1_len,str2_len);
