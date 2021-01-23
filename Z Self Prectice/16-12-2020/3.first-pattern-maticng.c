018660052f62cd21836ff061248dde0d0eb318aa
sajjad-njr <sajjadrahman1020@gmail.com>
2021-01-23 22:50:23 +0600

    first pattern matching

--- Z Self Prectice/16-12-2020/3.first-pattern-maticng.c
+++ Z Self Prectice/16-12-2020/3.first-pattern-maticng.c
@@ -4,8 +4,13 @@
 int main()
 {
     char str1[100],str2[100];
-    int str_len , str2_len;
+    int str1_len , str2_len;
 
     gets(str1);
     gets(str2);
+    str1_len = strlen(str1);
+    str2_len = strlen(str2);
+
+    printf(" %d %d ",str1_len,str2_len);
+    return 0;
 }
