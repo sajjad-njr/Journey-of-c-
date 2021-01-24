e2c4c800fa71683e081e6c0328f50b2280a7a594
sajjad-njr <sajjadrahman1020@gmail.com>
2021-01-24 20:21:26 +0600

    First Pattern Matching

--- Z Self Prectice/16-12-2020/5.string-first-pattern-match.c
+++ Z Self Prectice/16-12-2020/5.string-first-pattern-match.c
@@ -6,5 +6,11 @@ int main()
     char str1[100] , str2[100];
     gets(str1),gets(str2);
 
+    int str1_len = strlen(str1);
+    int str2_len = strlen(str2);
+
+    int max = str1_len - str2_len ;
+
 
 }
+
