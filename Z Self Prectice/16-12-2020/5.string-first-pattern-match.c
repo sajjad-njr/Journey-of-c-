8fe9f12bf0c4b4e27a2d778483cca83bb053030f
sajjad-njr <sajjadrahman1020@gmail.com>
2021-01-24 20:36:57 +0600

    First Pattern Matching

--- Z Self Prectice/16-12-2020/5.string-first-pattern-match.c
+++ Z Self Prectice/16-12-2020/5.string-first-pattern-match.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 #include<string.h>
 
+
 int main()
 {
     char str1[100] , str2[100];
@@ -9,9 +10,28 @@ int main()
     int str1_len = strlen(str1);
     int str2_len = strlen(str2);
 
-    int max = str1_len - str2_len ;
+    int max = str1_len - str2_len , k = 0 , i=0 ;
 
-    printf("\n\tTotal len = %d",max);
+    while(k<= max)
+    {
+        for( i = 0 ; i < str1_len ; i++)
+        {
+            if(str2[i] != str1[k+i-1])
+            {
+                k++;
+                break;
+            }
+        }
+        if(i == str2_len)
+        {
+            printf("\n\tItem is found = %d",k);
+        }
+    }
+    if(i != str2_len)
+    {
+        printf("\n\tItem is not found");
+    }
+   // printf("\n\tTotal len = %d",max);
     return 0;
 
 }
