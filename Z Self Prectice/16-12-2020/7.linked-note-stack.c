e7053178a3c6540fc13162303a95fd4916c554c1
sajjad-njr <sajjadrahman1020@gmail.com>
2021-01-25 20:37:55 +0600

    stack linked list

--- Z Self Prectice/16-12-2020/7.linked-note-stack.c
+++ Z Self Prectice/16-12-2020/7.linked-note-stack.c
@@ -6,4 +6,25 @@ typedef struct node
     int data;
     struct node *next;
 }Node;
+Node *top = 0;
 
+void push(int x)
+{
+    Node *new_node = (Node*)malloc(sizeof(Node));
+    //printf("\n\tEnter node Value = ");
+    //scanf("%d"&new_node->data);
+     new_node->data = x;
+    new_node->next = top;
+    top = new_node;
+
+
+}
+
+void display()
+{
+    Node *temp = top;
+    if(top == 0)
+    {
+        print
+    }
+}
