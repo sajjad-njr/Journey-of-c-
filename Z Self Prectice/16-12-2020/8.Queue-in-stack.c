ab60828fc50b10df1c5d9d270b6cfef5e3d7842c
sajjad-njr <sajjadrahman1020@gmail.com>
2021-01-25 22:45:12 +0600

    Queue with array

--- Z Self Prectice/16-12-2020/8.Queue-in-stack.c
+++ Z Self Prectice/16-12-2020/8.Queue-in-stack.c
@@ -6,4 +6,20 @@ int queue[N];
 int front = -1;
 int rare = -1;
 
-
+void enque(int x)
+{
+    if(rear = N - 1)
+    {
+        printf("\n\tOverflow ");
+    }
+    else if(front == -1 && rare == -1)
+    {
+        front = rare = 0;
+        queue[rare] = x;
+    }
+    else
+    {
+        rare++;
+        queue[rare] = x;
+    }
+}
