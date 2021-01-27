ad4d867dfe18a78702d86261160468c3a0690c70
sajjad-njr <sajjadrahman1020@gmail.com>
2021-01-27 22:31:27 +0600

    delete from start

---
+++ Z Self Prectice/1.Lab-preparation/3.Linked-LIST/7.DELETE-from-START.c
@@ -0,0 +1,65 @@
+#include<stdlib.h>
+#include<stdlib.h>
+
+typedef struct node
+{
+    int data;
+    struct node *next;
+
+} Node;
+
+Node *head = NULL;
+Node *tail = NULL;
+//linked List CREATE
+Node *createNode()
+{
+    Node *head = NULL;
+    Node *tail = NULL;
+
+    int num;
+
+    printf("\n\tEnter  (-1) to stop the Linked List =  ");
+    scanf("%d",&num);
+
+    while( num != -1)
+    {
+        Node *new_node = (Node*)malloc(sizeof(Node));
+
+        new_node->data = num;
+        new_node->next = NULL;
+
+        if(head == NULL)
+        {
+            head = new_node;
+            tail = new_node;
+        }
+        else
+        {
+            tail->next = new_node;
+            tail = new_node;
+        }
+
+        scanf("%d",&num);
+    }
+    return head;
+}
+
+void Delete_from_start()
+{
+    Node *temp;
+    temp = head;
+
+    head = head->next;
+    // free use ro clear garbage value
+    free(temp);
+}
+void display(Node *avail)
+{
+
+    while( avail != NULL)
+    {
+        printf(" %d ",avail->data);
+        avail = avail->next;
+    }
+
+}
