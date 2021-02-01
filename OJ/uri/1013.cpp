ff951ce5ebe692790054dbc36025e2ef90f456c2
sajjad-njr <sajjadrahman1020@gmail.com>
2021-02-01 23:26:11 +0600

    1013

--- OJ/uri/1013.cpp
+++ OJ/uri/1013.cpp
@@ -4,14 +4,14 @@ using namespace std;
 int main()
 {
     int a,b,c;
+
     cin>>a>>b>>c;
 
     int x = (a+b+abs(a-b))/2;
-    //cout << ab;
+
     int abc = (x+c+abs(x-c))/2;
+
     cout << abc;
 
-   // cout << a << b ;
-
-
+    return 0;
 }
