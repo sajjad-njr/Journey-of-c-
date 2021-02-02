a17f38486cbfdcf526617e90da70b92cf1bbb06f
sajjad-njr <sajjadrahman1020@gmail.com>
2021-02-02 22:15:13 +0600

    linked list

--- Z Self Prectice/2021_Feb/1.Linked_list.c
+++ Z Self Prectice/2021_Feb/1.Linked_list.c
@@ -5,6 +5,20 @@ typedef struct node
     int data;
     struct node *next;
 }Node;
+Node *head , *tail;
+
+Node *takeInput()
+{
+    Node *new_node ;
+
+    int n ;
+    scanf("%d",&n);
+
+    while(n--)
+    {
+
+    }
+}
 
 int main()
 {
