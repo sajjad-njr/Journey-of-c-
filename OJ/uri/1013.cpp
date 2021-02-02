e6e6b77fe8e3336a15963ae71f8039e47a581a48
sajjad-njr <sajjadrahman1020@gmail.com>
2021-02-02 22:12:40 +0600

    linked list

--- OJ/uri/1013.cpp
+++ OJ/uri/1013.cpp
@@ -3,11 +3,11 @@ using namespace std;
 
 int main()
 {
-    int a,b,c , x;
+    int a,b,c, x;
 
     cin>>a>>b>>c;
 
-     x = (a+b+abs(a-b))/2;
+    x = (a+b+abs(a-b))/2;
 
     x = (x+c+abs(x-c))/2;
 
