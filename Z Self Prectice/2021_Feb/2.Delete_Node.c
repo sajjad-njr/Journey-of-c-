25ff8474de79f42842df31d8cdaf8a1ae470ca5b
sajjad-njr <sajjadrahman1020@gmail.com>
2021-02-03 23:54:41 +0600

    delte node

---
+++ Z Self Prectice/2021_Feb/2.Delete_Node.c
@@ -0,0 +1,54 @@
+#include<stdio.h>
+#include<stdlib.h>
+typedef struct node
+{
+    int data;
+    struct node *next;
+} Node;
+Node *head, *tail;
+
+Node *takeInput()
+{
+    Node *new_node ;
+    head = NULL;
+    tail = NULL;
+
+    int n ;
+    scanf("%d",&n);
+
+    while(n--)
+    {
+        new_node = (Node*) malloc(sizeof(Node));
+        printf("\n\tEnter Node item = ");
+        scanf("%d",&new_node->data);
+        new_node->next = NULL;
+
+        if(head == NULL)
+        {
+            head = new_node;
+            tail = new_node;
+        }
+        else
+        {
+            tail->next = new_node;
+            tail = new_node;
+        }
+    }
+
+    return head;
+}
+
+void printttt(Node *a)
+{
+    while( a != NULL)
+    {
+        printf(" %d ",a->data);
+        a = a->next;
+    }
+}
+int main()
+{
+    Node *a = takeInput();
+     printttt(a);
+}
+
