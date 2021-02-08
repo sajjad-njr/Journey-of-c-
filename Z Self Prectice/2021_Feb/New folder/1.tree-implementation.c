8db6a8ee5d558276f819d86ef9917b5528eff76e
sajjad-njr <sajjadrahman1020@gmail.com>
2021-02-08 22:40:37 +0600

    tree implemention

--- Z Self Prectice/2021_Feb/New folder/1.tree-implementation.c
+++ Z Self Prectice/2021_Feb/New folder/1.tree-implementation.c
@@ -6,3 +6,21 @@ typedef struct node
     int data ;
     struct node *left, *right;
 } Node;
+
+Node *create()
+{
+    Node *new_node = (Node*)malloc(sizeof(Node));
+    int x;
+
+    printf("\n\tEnter -1 for no node = ");
+    scanf("%d",&x);
+
+    if(x == -1)
+    {
+       return 0;
+    }
+    new_node->data = x ;
+    printf("\nEnter the left child of = %d",x);
+    new_node->left = create();
+
+}
