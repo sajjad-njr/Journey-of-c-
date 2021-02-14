aeb1f132d8af6da93a11447a0c13f2562967ede6
sajjad-njr <sajjadrahman1020@gmail.com>
2021-02-14 09:34:48 +0600

    sum of all number in array

--- DS_Udemy/A_Z_mystry/Array/1.sum_of_all_array.cpp
+++ DS_Udemy/A_Z_mystry/Array/1.sum_of_all_array.cpp
@@ -1,1 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+int main()
+{
+    cout<<"Enter the size of Array = ";
+    int n; cin>>n;
+    cout<<endl;
+
+    int A[n];
+    cout <<"Enter the element of array = ";
+
+    for(int i = 0 ; i < n ; i++)
+    {
+        cin >> A[i];
+    }
+    int sum = 0 ;
+
+    for(int i = 0 ; i < n ; i++)
+    {
+         sum += A[i];
+    }
+
+    cout << " Total sum of array = " << sum ;
+    return 0;
+}
