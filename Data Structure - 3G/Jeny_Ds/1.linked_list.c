0168e7f15494bc0739eeeb5afcae6476bac0062e
sajjad-njr <sajjadrahman1020@gmail.com>
2021-02-15 21:00:56 +0600

    ll

---
+++ Data Structure - 3G/Jeny_Ds/1.linked_list.c
@@ -0,0 +1,56 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct node
+{
+    int data;
+    struct node *next;
+}Node;
+
+Node *head, *tail;
+
+Node *creteNode()
+{
+    head = NULL;
+    tail = NULL;
+
+    int n;
+    scanf("%d",&n);
+    while(n--)
+    {
+        Node *new_node =(Node*)malloc(sizeof(Node));
+        printf("\n\tEnter The value of Node = ");
+        scanf("%d",new_node->data);
+        new_node->next = NULL;
+
+        if(head == NULL)
+        {
+            head = new_node;
+            tail = new_node;
+        }
+        else
+        {
+            tail = tail->next;
+            tail = new_node;
+
+        }
+    }
+    return head;
+}
+
+void p(Node *head)
+{
+    while(head != NULL)
+    {
+        printf(" %d ",head->data);
+        head = head->next;
+    }
+}
+
+int main()
+{
+    Node *i = creteNode();
+    p(i);
+
+    return 0;
+}
