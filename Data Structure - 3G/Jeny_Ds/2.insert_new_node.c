16607f90d74b37b8469084f25564a444f6882f79
sajjad-njr <sajjadrahman1020@gmail.com>
2021-02-16 13:52:14 +0600

    Insert Node in Linked list

--- Data Structure - 3G/Jeny_Ds/2.insert_new_node.c
+++ Data Structure - 3G/Jeny_Ds/2.insert_new_node.c
@@ -6,14 +6,15 @@ typedef struct node
     int data;
     struct node *next;
 
-}Node;
+} Node;
+Node *head, *tail;
 
 Node *createNode()
 {
     int n;
     printf("Enter the value for stop -1 = ");
     scanf("%d",&n);
-    Node *head , *tail;
+    //Node *head, *tail;
 
     head = NULL;
     tail = NULL;
@@ -21,9 +22,9 @@ Node *createNode()
     while(n != -1)
     {
         Node *new_node = (Node*) malloc(sizeof(Node));
-       // printf("\n\tEnter the value = ");
-       // scanf("%d",&new_node->data);
-       new_node->data = n;
+        // printf("\n\tEnter the value = ");
+        // scanf("%d",&new_node->data);
+        new_node->data = n;
         new_node->next = NULL;
 
         if(head == NULL)
@@ -40,6 +41,31 @@ Node *createNode()
     }
     return head;
 }
+
+Node *insertFirst( )
+{
+    Node *avail ;
+
+    avail = (Node*)malloc(sizeof(Node));
+    //avail= head;
+
+    printf("\n\tEnter  First Position Node  the value = ");
+    scanf("%d",&avail->data);
+    //new_node->data = n;
+    avail->next = NULL;
+
+    if( head == NULL)
+    {
+        printf("\nOverflow\n");
+    }
+    else
+    {
+        avail->next = head;
+        head = avail;
+    }
+    return head;
+}
+
 void p(Node *q)
 {
     while( q != NULL)
@@ -52,6 +78,9 @@ void p(Node *q)
 int main()
 {
     Node *i = createNode();
+
+    i = insertFirst();
+
     p(i);
 
     return 0;
