04253fd167413fda3a376cc82fd00a4ebad15e0d
sajjad-njr <sajjadrahman1020@gmail.com>
2021-02-17 19:13:43 +0600

    6 no (ii)

--- Data Structure - 3G/Jeny_Ds/Doubly Linked List/6_no.c
+++ Data Structure - 3G/Jeny_Ds/Doubly Linked List/6_no.c
@@ -2,21 +2,21 @@
 #include <conio.h>
 #define MAX_LEN 10
 
-void l_search_recursive(int l[],int num,int ele);
-void l_search_nonrecursive(int l[],int num,int ele);
-void read_list(int l[],int num);
-void print_list(int l[],int num);
+void l_search_recursive(int array[],int num,int ele);
+void l_search_nonrecursive(int array[],int num,int ele);
+void read_list(int array[],int num);
+void print_list(int array[],int num);
 
-/* Non-Recursive function*/
-void b_search_nonrecursive(int l[],int num,int ele);
-/* Recursive function*/
-int b_search_recursive(int l[],int arrayStart,int arrayEnd,int a);
+/* Non-Recursive function Binary search */
+void b_search_nonrecursive(int array[],int num,int ele);
+/* Recursive function Binary search */
+int b_search_recursive(int array[],int arrayStart,int arrayEnd,int a);
 
 
 int main()
 {
-    int l[MAX_LEN], num, ele;
-    int ch , pos;
+    int array[MAX_LEN], num, ele;
+    int data , pos;
 
     printf("======================================================");
     printf("\n\t\t\tMENU");
@@ -25,37 +25,37 @@ int main()
     printf("\n[2] Linary Search using Non-Recursion method");
     printf("\n[3] Binary Search using Recursion method");
     printf("\n[4] Binary Search using Non-Recursion method");
-    printf("\n\nEnter your Choice:");
-    scanf("%d",&ch);
+    printf("\n\nEnter your Choice: ");
+    scanf("%d",&data);
 
-    if(ch <= 4 & ch>0)
+    if(data <= 4 & data>0)
     {
         printf("Enter the number of elements :");
         scanf("%d",&num);
-        read_list(l,num);
+        read_list(array,num);
 
         printf("\nElements present in the list are:\n\n");
-        print_list(l,num);
+        print_list(array,num);
 
         printf("\n\nElement you want to search:\n\n");
         scanf("%d",&ele);
 
-        switch(ch)
+        switch(data)
         {
         case 1:
             printf("\n**Recursion method**\n");
-            l_search_recursive(l,num,ele);
+            l_search_recursive(array,num,ele);
             getch();
             break;
 
         case 2:
             printf("\n**Non-Recursion method**\n");
-            l_search_nonrecursive(l,num,ele);
+            l_search_nonrecursive(array,num,ele);
             getch();
             break;
         case 3:
             printf("\nRecursive method:\n");
-            pos=b_search_recursive(l,0,num,ele);
+            pos=b_search_recursive(array,0,num,ele);
             if(pos==-1)
             {
                 printf("Element is not found");
@@ -69,7 +69,7 @@ int main()
 
         case 4:
             printf("\nNon-Recursive method:\n");
-            b_search_nonrecursive(l,num,ele);
+            b_search_nonrecursive(array,num,ele);
             //getch();
             break;
         }
@@ -78,11 +78,11 @@ int main()
 
 //Recursive method
 
-void l_search_recursive(int l[],int num,int ele)
+void l_search_recursive(int array[],int num,int ele)
 {
     int f = 0;
 
-    if( l[num] == ele)
+    if( array[num] == ele)
     {
         printf("\nThe element %d is present at position %d in list\n",ele,num+1);
         f=1;
@@ -95,18 +95,18 @@ void l_search_recursive(int l[],int num,int ele)
         }
         else
         {
-            l_search_nonrecursive(l,num-1,ele);
+            l_search_nonrecursive(array,num-1,ele);
         }
     }
     getch();
 }
  //Non-Recursive method
 
-void l_search_nonrecursive(int l[],int num,int ele)
+void l_search_nonrecursive(int array[],int num,int ele)
 {
     int j, f=0;
     for(j=0; j<num; j++)
-        if( l[j] == ele)
+        if( array[j] == ele)
         {
             printf("\nThe element %d is present at position %d in list\n",ele,j+1);
             f=1;
@@ -117,7 +117,7 @@ void l_search_nonrecursive(int l[],int num,int ele)
 }
 
 /* Non-Recursive function*/
-void b_search_nonrecursive(int l[],int num,int ele)
+void b_search_nonrecursive(int array[],int num,int ele)
 {
    int l1,i,j, flag = 0;
    l1 = 0;
@@ -125,14 +125,14 @@ void b_search_nonrecursive(int l[],int num,int ele)
    while(l1 <= i)
    {
       j = (l1+i)/2;
-      if( l[j] == ele)
+      if( array[j] == ele)
       {
     printf("\nThe element %d is present at position %d in list\n",ele,j+1);
          flag =1;
          break;
       }
       else
-        if(l[j] < ele)
+        if(array[j] < ele)
            l1 = j+1;
         else
            i = j-1;
@@ -142,34 +142,34 @@ void b_search_nonrecursive(int l[],int num,int ele)
 }
 
 /* Recursive function*/
-int b_search_recursive(int l[],int arrayStart,int arrayEnd,int a)
+int b_search_recursive(int array[],int arrayStart,int arrayEnd,int a)
 {
   int m,pos;
   if (arrayStart<=arrayEnd)
   {
     m=(arrayStart+arrayEnd)/2;
-    if (l[m]==a)
+    if (array[m]==a)
       return m;
-    else if (a<l[m])
-      return b_search_recursive(l,arrayStart,m-1,a);
+    else if (a<array[m])
+      return b_search_recursive(array,arrayStart,m-1,a);
     else
-      return b_search_recursive(l,m+1,arrayEnd,a);
+      return b_search_recursive(array,m+1,arrayEnd,a);
    }
    return -1;
 }
 
-void read_list(int l[],int num)
+void read_list(int array[],int num)
 {
     int j;
     printf("\nEnter the elements:\n");
     for(j=0; j<num; j++)
-        scanf("%d",&l[j]);
+        scanf("%d",&array[j]);
 }
 
-void print_list(int l[],int num)
+void print_list(int array[],int num)
 {
     int j;
     for(j=0; j<num; j++)
-        printf("%d\t",l[j]);
+        printf("%d\t",array[j]);
 }
 
