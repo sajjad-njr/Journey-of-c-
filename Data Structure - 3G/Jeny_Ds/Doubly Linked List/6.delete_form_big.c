79714fcc7bdbbf88bab657add8449e96743c6881
sajjad-njr <sajjadrahman1020@gmail.com>
2021-02-17 19:20:01 +0600

    delete from starr

---
+++ Data Structure - 3G/Jeny_Ds/Doubly Linked List/6.delete_form_big.c
@@ -0,0 +1,80 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct node
+{
+    int data;
+
+    struct node *prev;
+    struct node *next;
+} Node;
+
+Node *head = NULL, *tail = NULL;
+
+Node *createList()
+{
+    int n ;
+    printf("\n\tEnter -1 for stop the loop = ");
+    scanf("%d",&n);
+
+    while(n != -1)
+    {
+        Node *new_node = (Node*)malloc(sizeof(Node));
+        new_node->data = n;
+        new_node->prev = NULL;
+        new_node->next = NULL;
+
+        if(head == NULL)
+        {
+            head = tail = new_node;
+        }
+        else
+        {
+            tail->next = new_node;
+            new_node->prev = tail;
+            tail = new_node;
+        }
+        scanf("%d",&n);
+    }
+    return head;
+}
+
+Node *deleteFromStart()
+{
+    Node *avail ;
+
+    if(head == NULL)
+    {
+        printf("\n\tThe list has no NODE");
+
+    }
+    else
+    {
+        avail = head ;
+        head = head->next;
+        head->prev = NULL;
+        free(avail);
+    }
+    return head;
+}
+
+void a(Node *i)
+{
+    i = head;
+
+    while(i != NULL)
+    {
+        printf(" %d ",i->data);
+        i = i->next;
+    }
+}
+int main()
+{
+    Node *aa = createList();
+    aa =  deleteFromStart();
+    a(aa);
+
+    return 0;
+}
+
+
