514fc17450c8e0cab9615eeb6504f0d621c38001
sajjad-njr <sajjadrahman1020@gmail.com>
2021-02-20 22:11:16 +0600

    pattern matching

--- Z Self Prectice/0-Lab_Exam/string/1.pattern_matcing.c
+++ Z Self Prectice/0-Lab_Exam/string/1.pattern_matcing.c
@@ -5,21 +5,23 @@
 int main()
 {
     char str1[100],str2[100];
+    int  k = 0, i = 0,len1 = 0, len2 = 0, max = 0;
     printf("\n\tEnter Main string  = ");
     gets(str1);
     printf("\n\tEnter Password string = ");
     gets(str2);
 
-    int  k = 0, i = 0,len1=0, len2=0, max = 0;
-    len1 = strlen(str1);
-    len2 = strlen(str2);
-    max = len1-len2;
 
-    while(k < max)
+    len1 = strlen(str1);
+    printf("\nstring len = %d",len1);
+    len2 = strlen(str2);
+    max = len1 - len2;
+
+    while(k <= max)
     {
         for( i = 0 ; i < len1 ; i++)
         {
-            if(str2[i] != str2[k+i-1])
+            if(str2[i] != str1[k+i])
             {
                 k++;
                 break;
@@ -27,12 +29,13 @@ int main()
         }
         if( i == len2)
         {
-            printf("\n\tPattern Match at position = %d",i);
+            printf("\n\tPattern Match at position = %d",k-1);
             break;
         }
     }
+   // printf("\n\tEnter Password string = ");
     if(i != len2)
-        printf("\n\tPattern Match at position = ",);
+        printf("\n\tPattern not Match ");
 
     //puts(str1);
     //puts(str2);
