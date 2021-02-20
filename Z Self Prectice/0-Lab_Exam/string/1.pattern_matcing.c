530abdbf9f020d224644f6d89b69aa2e6782efcf
sajjad-njr <sajjadrahman1020@gmail.com>
2021-02-20 21:20:22 +0600

    pattern matching

---
+++ Z Self Prectice/0-Lab_Exam/string/1.pattern_matcing.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    char str1[100],str2[100];
+    printf("\n\tEnter Main and Password Sstring = ");
+    gets(str1);
+    gets(str2);
+
+    puts(str1);
+    puts(str2);
+
+    return 0;
+}
