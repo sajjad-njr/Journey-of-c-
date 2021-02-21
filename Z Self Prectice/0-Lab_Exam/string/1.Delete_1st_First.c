318618f6702e1904a9b67bdb634291d5edb549bb
sajjad-njr <sajjadrahman1020@gmail.com>
2021-02-21 08:51:14 +0600

    delete from 1st position

---
+++ Z Self Prectice/0-Lab_Exam/string/1.Delete_1st_First.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main()
+{
+    int i = 0 , size;
+    printf("\n\tEnter size of array = ");
+    scanf("%d",&size);
+
+    int ARRAY[size];
+
+    for(i = 0 ; i < size ; i++)
+    {
+        scanf("%d",&ARRAY[i]);
+    }
+
+     //increment index value i = 1
+
+     for(i = 1 ; i < size ; i++)
+    {
+        printf(" %d ",ARRAY[i]);
+    }
+    return 0;
+}
