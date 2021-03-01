190cbd7516e78734ab070bfb09ee9ba028067192
sajjad-njr <sajjadrahman1020@gmail.com>
2021-03-01 22:45:10 +0600

    algorithm-linked list

--- ALGORITHM/1.  March - 2021/1.Linked-list.c
+++ ALGORITHM/1.  March - 2021/1.Linked-list.c
@@ -9,4 +9,25 @@ typedef struct node
 
 Node *head = NULL , *tail = NULL;
 
+Node *createNode()
+{
+    int n ;
+    scanf("%d",&n);
+
+    while(n != -1)
+    {
+        Node *newNode = (Node*)malloc(sizeof(Node));
+        newNode->data = n;
+        newNode->next = NULL;
+
+        if(head == NULL)
+        {
+            head = tail= newNode;
+        }
+        else
+        {
+
+        }
+    }
+}
 
