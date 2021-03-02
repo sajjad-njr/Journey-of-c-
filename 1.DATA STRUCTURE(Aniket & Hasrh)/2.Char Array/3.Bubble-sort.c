028da89c2e555490a132f212a7aa532c6d61c8e0
sajjad-njr <sajjadrahman1020@gmail.com>
2021-03-02 23:08:50 +0600

    Bubble sort

--- 1.DATA STRUCTURE(Aniket & Hasrh)/2.Char Array/3.Bubble-sort.c
+++ 1.DATA STRUCTURE(Aniket & Hasrh)/2.Char Array/3.Bubble-sort.c
@@ -2,10 +2,22 @@
 
 int main()
 {
-    int n , j , temp;
+    int n, j, temp;
     printf("\nEnter the size of Array = ");
     scanf("%d",&n);
 
     int A[n];
+
+    for(int i = 0 ; i < n ; i++)
+    {
+        scanf("%d",&A[i]);
+    }
+
+    for(int i = 0 ; i < n ; i++)
+    {
+        printf(" %d ", A[i]);
+    }
+
+    return 0;
 
 }
